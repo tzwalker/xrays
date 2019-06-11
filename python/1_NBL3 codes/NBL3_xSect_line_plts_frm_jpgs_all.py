6aa974dc800d5072f39303c9d0e0a2b9cf460c25
tzwalker <tzwalker@asu.edu>
2019-06-11 15:30:01 -0700

    added quick-dirty export to csv fxn in img_processing

--- python/1_NBL3 codes/NBL3_xSect_line_plts_frm_jpgs_all.py
+++ python/1_NBL3 codes/NBL3_xSect_line_plts_frm_jpgs_all.py
@@ -30,6 +30,13 @@ sam1 = {'Name': 'NBL3_1', 'Rot': 0, 'depth': 12, 'depth_pixels': 175, 'channels'
 sam2 = {'Name': 'NBL3_3', 'Rot': 5, 'depth': 17, 'depth_pixels': 200, 'channels': ['XBIC', 'Cd', 'Te', 'Cu'], 'channel_maxes': [0.9, 1600,1600, 1200]}
 sam3 = {'Name': 'TS58A', 'Rot': 0.5, 'depth': 11, 'depth_pixels': 85, 'channels': ['XBIC', 'Cd', 'Te', 'Cu'], 'channel_maxes': [0.5, 1300,1300, 400]}
 
+# quick-and-dirty export Cu and XBIC line profiles to CSVs for PVSC poster 
+# =============================================================================
+# sam1 = {'Name': 'NBL3_1', 'Rot': 0, 'depth': 12, 'depth_pixels': 175, 'channels': ['XBIC', 'Cu'], 'channel_maxes': [6, 350]}
+# sam2 = {'Name': 'NBL3_3', 'Rot': 5, 'depth': 17, 'depth_pixels': 200, 'channels': ['XBIC', 'Cu'], 'channel_maxes': [0.9, 1200]}
+# =============================================================================
+
+
 samples = [sam1, sam2]#, sam3]
 
 
@@ -68,27 +75,36 @@ def rotate_and_integrate_cross_section(samples):
 master_list = rotate_and_integrate_cross_section(samples)
 
 def plot_save_combined_channels(samples, master_list):
+    exes = []
     for sample, set_of_lines in zip(samples, master_list):
         x = sample['depth']
         x_res = sample['depth_pixels']
         x_calib = np.linspace(0, x, x_res)
+        exes.append(x_calib)
         
         lower_bound = min(x_calib)
         upper_bound = max(x_calib)
         
         #for line in set_of_lines: #not sure if this is necessary for the time being... just roll with below for now.
         fig, ax1 = plt.subplots()
+        axis_title_size = 20
+        tick_size = 16
+        line_size = 5
         
-        ax1.set_xlabel('Depth (um)', fontsize = 18)
-        ax1.tick_params(axis='x', labelsize = 14)
+        ax1.set_xlabel('Depth (um)', fontsize = axis_title_size)
+        ax1.tick_params(axis='x', labelsize = tick_size)
         
         color = 'tab:red'
-        lns1 = ax1.plot(x_calib, set_of_lines[0], color=color, label = 'XBIC')      #XBIC line
-        ax1.set_ylabel('XBIC (nA)', color=color, fontsize = 18)
-        ax1.tick_params(axis='y', labelcolor=color, labelsize = 14)
+        lns1 = ax1.plot(x_calib, set_of_lines[0], color=color, label = 'XBIC', linewidth=line_size)      #XBIC line
+        ax1.set_ylabel('XBIC (nA)', color=color, fontsize = axis_title_size)
+        ax1.tick_params(axis='y', labelcolor=color, labelsize = tick_size)
         
+        ax2 = ax1.twinx()
+        color2 = 'tab:orange'
+        ax2.set_ylabel('Cu XRF (a.u.)', fontsize = axis_title_size)
+        ax2.tick_params(axis='y', labelcolor=color2, labelsize = tick_size)
+        ax2.set_ylim(0,1300)
 # =============================================================================
-#         ax2 = ax1.twinx()
 #         color = 'tab:blue'
 #         lns2 = ax2.plot(x_calib, set_of_lines[1], color=color, linestyle = '--', label = 'Cd')  #Cd line
 #         ax2.set_ylabel('a.u.', color=color, fontsize = 18)
@@ -97,80 +113,57 @@ def plot_save_combined_channels(samples, master_list):
 #         #ax2 = ax1.twinx()
 #         color = 'tab:gray'
 #         lns3 = ax2.plot(x_calib, set_of_lines[2], color=color, linestyle = '--', label = 'Te')  #Cd line
-# # =============================================================================
-# #         ax2.set_ylabel('a.u.', color=color, fontsize = 18)
-# #         ax2.tick_params(axis='y', labelcolor=color, labelsize = 14)
-# # =============================================================================
-#         
-#         color = 'tab:orange'
-#         lns4 = ax2.plot(x_calib, set_of_lines[3], color=color, linestyle = '--', label = 'Cu')        #Cu line
-#             
-#         # from https://stackoverflow.com/questions/5484922/secondary-axis-with-twinx-how-to-add-to-legend
-#         lns = lns1+lns2+lns3 +lns4
-#         labs = [l.get_label() for l in lns]
-#         ax1.legend(lns, labs, loc=0, fontsize = 12)
 # =============================================================================
         
+        lns4 = ax2.plot(x_calib, set_of_lines[3], color=color2, linestyle = '--', label = 'Cu', linewidth=line_size)        #Cu line
+            
+        # from https://stackoverflow.com/questions/5484922/secondary-axis-with-twinx-how-to-add-to-legend
+        lns = lns1 + lns4
+        labs = [l.get_label() for l in lns]
+        leg = ax1.legend(lns, labs, loc='lower center', fontsize = tick_size)
+        
+        for line in leg.get_lines():
+            line.set_linewidth(4)
+        
         fig.tight_layout()                          # otherwise the right y-label is slightly clipped
-        plt.title(sample['Name'], fontsize = 18)
+        plt.title(sample['Name'], fontsize = axis_title_size)
         plt.xlim([lower_bound, upper_bound])
         ax1.grid()
         plt.show()
     
-        fig.get_figure()
-        fig.savefig(save_path + r"\{s}, XBIC_Cu_Cd comb.jpg".format(s = sample["Name"]))
-    return
+# =============================================================================
+#         fig.get_figure()
+#         fig.savefig(save_path + r"\{s}, XBIC_Cu_Cd comb.jpg".format(s = sample["Name"]))
+# =============================================================================
+    return #exes (PVSC)
 
 plot_save_combined_channels(samples, master_list)
 
-# =============================================================================
-#             #calibrates line plot x axis
-#             x = sample['depth']                                                             #get the thickness of the sample (read from original image)
-#             x_res = sample['depth_pixels']                                                  #get pixels in x of screenshot (read from Windows explorer)
-#             
-#             x_calib = np.linspace(0,x,x_res)                                                #generate an array of numbers from xmin to xmax that are spaced by the number of x-pixels in the image (will be used to plot Y against)
-#             lower_bound = min(x_calib)
-#             upper_bound = max(x_calib)
-#             
-#             #calibrate line plot y axis
-#             #by default the integration returns a sum of the pixels in a column that corresponds to an intensity 
-#             #the intensity is determined by rgb2gray() when converting to greyscale
-#             #the greyscale intensity will be converted to XRF intensity estimated from the ch_max of the cross-section image 
-#             y_max = max(column_sum)                                                         #gets the maximum intensity from the integration of the rotated image
-#             channel_calib_factor = ch_max / y_max                                           #get scaling factor that will convert greyscale intenistyto XRF intensity
-#             scaled_column_sum = column_sum * channel_calib_factor                           #convert             
-#             
-#             if channel == 'XBIC':
-#                 units = 'nA'
-#             else:
-#                 units = 'a.u.'
-#             
-#             plt.plot(x_calib, scaled_column_sum)                            
-#             plt.xlim([lower_bound, upper_bound])
-#             
-#             #line plot settings
-#             plt.title(image_name, fontsize = 18)
-#             plt.xlabel('X (um)', fontsize = 18)
-#             plt.xticks(rotation = 0)
-#             plt.ylabel(channel + ' ' + '(' + units + ')', fontsize = 18)
-#             plt.yticks(rotation = 0)
-#             plt.tick_params(axis="both", labelsize = 15)
-#             
-#             plt.grid()                      #places gridlines for prettification
-#         plt.show()                      #push this line to outer loop to make plots of all channels (rememebr to rename plt-titles); push line to inner loop to make individual plots for each channel
-#             #fig.get_figure()
-#             #fig.savefig(save_path + r"\{s}, {e}.jpg".format(s = sample["Name"], e = channel))
-# =============================================================================
-            
-            
-            
-            
+### EXTRA CODE
 
+# quick-and-dirty export Cu and XBIC line profiles to CSVs for PVSC poster
 # =============================================================================
-# #this was written to batch convert color images to greyscale to avoid having to do so manually in DigitialMicrograph (DM) software
-# #it works, but the image canno tbe read by DM 
+# calib_xs = plot_save_combined_channels(samples, master_list)
+# 
+# arrays = []
+# for sample, x in zip(master_list, calib_xs):
+#     for channel in sample:
+#         a = [x, channel]
+#         a = np.asmatrix(a)
+#         a = np.transpose(a)
+#         arrays.append(a)
+# count = 0      
+# for array in arrays:
+#     np.savetxt(r'C:\Users\Trumann\Dropbox (ASU)\DefectLab\PVSC 46 -2019\foo' + str(count) + '.csv', array, delimiter = ',')
+#     count = count +1 
+# =============================================================================
+
+# batch convert color to greyscale without DigitialMicrograph (DM) software
+# =============================================================================
+# #cannot tbe read by DM 
 # #error: "sorry, cannot handle contiguous data with PhotometricInterpretation=1 and Samepixels=2"
-# #however, the image was about to be opened in GIMP, so it may prove ot be useful at some point
+# #
+# #the image was about to be opened in GIMP, so it may prove ot be useful at some point
 #     
 # #img_num generates a numeric indiactor matching the filename (which may change)
 # color_imgs_path = r'C:\Users\Trumann\Desktop\solo_images_jpgs'
