03126dd896acbf37e4ea476406896737c17688b7
tzwalker <tzwalker@asu.edu>
2019-06-25 16:01:12 -0700

    adding greater flexibility to import code

--- python/personal-programs/a_begin_importH5.py
+++ python/personal-programs/a_begin_importH5.py
@@ -1,43 +1,46 @@
 import matplotlib.pyplot as plt
 
+get_defs = 'work'
+
 import sys
-custom_def_path = '/home/kineticcross/Desktop/xrays/python/personal-programs/twalker_defs'
+if get_defs == 'work':
+    custom_def_path = r'C:\Users\Trumann\Desktop\xrays\python\personal-programs\twalker_defs'
+else:
+    custom_def_path = '/home/kineticcross/Desktop/xrays/python/personal-programs'
+
 sys.path.append(custom_def_path)
 
 import import_maps_H5
 import a_begin_absorb
 
-scan_path = '/home/kineticcross/Desktop/xrays/python/personal-programs'
+if get_defs == 'work':
+    scan_path = r'C:\Users\Trumann\Desktop\2019_03_2IDD_NBL3\img.dat'
+else:
+    scan_path = '/home/kineticcross/Desktop/xrays/python/personal-programs'
 
 scans = ['264', '422', '385']
 energy = [8.99, 8.99, 8.99]         #keV
 
 ### stack structure (UPTREAM LAYER FIRST, DOWNSTREAM LAYER LAST) ###
-STACK = ['Mo', 'ZnTe', 'CdTe', 'CdS', 'SnO2']
-layer_density = [10.2, 6.34, 5.85, 4.82, 6.85]
+STACKS = [ ['Mo', 'ZnTe', 'CdTe', 'CdS', 'SnO2'],
+         ['Au', 'CdTe', 'CdS', 'SnO2'],
+         ['Mo', 'ZnTe', 'CdTe', 'CdS', 'SnO2'] ]
+
+lay_densities = [ [10.2, 6.34, 5.85, 4.82, 6.85],
+                 [19.3, 5.85, 4.82, 6.85],
+                 [10.2, 6.34, 5.85, 4.82, 6.85] ]
 
 ### enter thickness in centimeter of each layer for the sample in each scan ###
 # number of items in each of these lists needs to equal number of scans.samples being studied
-thk_Mok = [500E-7, 500E-7, 500E-7]
-thk_ZnTe = [375E-7, 375E-7, 375E-7]
-thk_CdTe = [4E-4, 5E-4, 6E-4]          
-thk_CdS = [80E-7, 80E-7, 80E-7]
-thk_Sno2 = [100E-7, 100E-7, 100E-7]
-
+lay_thicks = [ [500E-7, 375E-7, 4E-4, 80E-7, 100E-7], 
+              [500E-7, 375E-7,  5E-4, 80E-7, 100E-7], 
+              [500E-7, 375E-7,  6E-4, 80E-7, 100E-7] ]
 
 #files = import_maps_H5.import_h5s(scans, scan_path)
 
 
-
-#ChOIs = channels_of_interest
-ChOIs = ['Cd_L', 'Te_L', 'Cu']
-
-#master_index_list = import_maps_H5.get_ChOIs_for_all_scans(files, ChOIs)
-
-
-
-#this function uses the element indices from the master_index_list to extract the 2D fitted data arrays from the H5 file
-#it also build a master list that contains the 2D numpy arrays of interest, rather than just the indices
+# this function uses the element indices from the master_index_list to extract the 2D fitted data arrays from the H5 file
+# it also build a master list that contains the 2D numpy arrays of interest, rather than just the indices
 def extract_maps(H5s, list_of_lists):
     maps = []                                                       #initialize master list
     for H5, channel_indices in zip(H5s, list_of_lists):
@@ -58,11 +61,22 @@ import stk_info
 beam_theta = np.sin(90*np.pi/180)                                                     #angle between beam and sample normal
 detect_theta = np.sin(47*np.pi/180)                                                   #angle between detector and sample normal
 
-stack_for_each_sample_list = []
+what_will_this_be = stk_info.get_stack_info(STACKS, lay_densities, lay_thicks, energy)
 
-for scan, en, tm, tz, tcd, #tc, ts in zip(scans, energy, thk_Mok, thk_ZnTe, thk_CdTe, thk_CdS, thk_Sno2):
-    stack_info = stk_info.get_stack#_info(STACK, layer_density, energy)    
-    stack_for_each_sample_list.append(stack_info)
+
+
+
+
+#ChOIs = channels_of_interest
+ChOIs = ['Cd_L', 'Te_L', 'Cu']
+
+#master_index_list = import_maps_H5.get_ChOIs_for_all_scans(files, ChOIs)
+
+# =============================================================================
+# for scan, en, tm, tz, tcd, #tc, ts in zip(scans, energy, thk_Mok, thk_ZnTe, thk_CdTe, thk_CdS, thk_Sno2):
+#     stack_info = stk_info.get_stack#_info(STACK, layer_density, energy)    
+#     stack_for_each_sample_list.append(stack_info)
+# =============================================================================
     
 
 #Zs = a_begin_absorb.channel_to_atomic_num(ChOIs)
--- python/personal-programs/twalker_defs/stk_info.py
+++ python/personal-programs/twalker_defs/stk_info.py
@@ -1,25 +1,26 @@
 import xraylib as xl
-#returns a list of dictionaries
-#each dictionary contains information on the layer to be used in the absorption correction
-def get_stack_info(STACK, layer_density, E):
-    STACK_dicts = []
-    for layer, dens in zip(STACK, layer_density):
-        layer_dict = dict()
-        key = 'Name'
-        layer_dict.setdefault(key, layer)
+
+
+def get_stack_info(STACKS, dens, T, E):
+    stack_whole_info = []
+    for S, D, t, e in zip(STACKS, dens, T, E):
+        stack_layer_infos = []
+        for layer, density, thickness in zip(S, D, t): 
+            scan_layer_dict = dict()
+            key = 'Name'
+            scan_layer_dict.setdefault(key, layer)
+            
+            key = 'rho'
+            scan_layer_dict.setdefault(key, density)
         
-        key = 'rho'
-        layer_dict.setdefault(key, dens)
-        
-        key = 'cap-x-sect'
-        capture = xl.CS_Total_CP(layer, E)
-        layer_dict.setdefault(key, capture)
-        
-        for ###
-        key = 'thick'
-        layer_dict.setdefault(key, )
-        
-        layer_info = xl.CompoundParser(layer)
+            key = 'cap-x-sect'
+            capture = xl.CS_Total_CP(layer, e)
+            scan_layer_dict.setdefault(key, capture)
+    
+            key = 'thick'
+            scan_layer_dict.setdefault(key, thickness)
+            
+            layer_info = xl.CompoundParser(layer)
         #example for SnO2
         #layer_info = {'nElements': 2, 
                         #'nAtomsAll': 3.0, 
@@ -27,10 +28,11 @@ def get_stack_info(STACK, layer_density, E):
                         #'massFractions': [0.21235649346340169, 0.7876435065365983], 
                         #'nAtoms': [2.0, 1.0], 
                         #'molarMass': 150.69}
-        layer_dict.update(layer_info)
+            scan_layer_dict.update(layer_info)
+            stack_layer_infos.append(scan_layer_dict)
         
-        STACK_dicts.append(layer_dict)
-    return STACK_dicts
+        stack_whole_info.append(stack_layer_infos)
+    return stack_whole_info
 
 
 #ahhh!!! if i want to make a code that can adjust for different thicknesses of CdTe
