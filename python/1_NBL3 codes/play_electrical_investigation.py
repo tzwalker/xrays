efe0fa2efa051f7eee72836ddd1196cd2e5a674c
tzwalker <tzwalker@asu.edu>
2019-07-08 17:14:13 -0700

    some comments to NBL3 elect

--- python/1_NBL3 codes/play_electrical_investigation.py
+++ python/1_NBL3 codes/play_electrical_investigation.py
@@ -28,7 +28,7 @@ XBIC_h5s = import_maps_H5.import_h5s(XBIC_scans, scan_path)
 
 h5_filename = r'C:\Users\Trumann\Desktop\2017_12_2018_07_NBL3_bacth_refit\img.dat\2idd_0440.h5'
 
-H5 = h5.File(h5_filename, 'r')
+H5 = h5.File(h5_filename, 'r') #make sure this has 'r', otherwise py modulse will erae contents of file!
 
 XBIC_name = H5['/MAPS/scaler_names'][2] #index of this identifier will be the same as the index of the desired map
 XBIC_cts = H5['/MAPS/scalers'][2] #actual XBIC map
