0e41c6216e3c470ed98566786e09319f1542a8c6
gr4yh4t <gr4yh4t@protonmail.com>
2019-07-30 20:57:35 -0700

    conceptually resolved plotting axes ticklabels problem

--- python/1_NBL3_code/a_start_planV.py
+++ python/1_NBL3_code/a_start_planV.py
@@ -1,4 +1,4 @@
-get_defs = 'work'
+get_defs = 'wor'
 ### paths for custom defintion files and scans ### change according to the operating system environment
 if get_defs == 'work':
     scan_path = r'C:\Users\Trumann\Desktop\NBL3_data\all_H5s'
@@ -69,5 +69,7 @@ e_statistics.standardize_channels(samples) # these should have same structure as
 
 import z_plotting
 
-float_axis_list = z_plotting.plot_elect_maps(samples, 'XBIC')
+z_plotting.plot_elect_maps(samples, 'XBIC')
+
+
 
--- python/1_NBL3_code/z_plotting.py
+++ python/1_NBL3_code/z_plotting.py
@@ -6,44 +6,61 @@ import pandas as pd
 num_ticks = 10
 
 def get_real_coordinates(x_axis, y_axis):
-    xpixsize = (max(x_axis) - min(x_axis)) / len(x_axis) # in um
-    x_i = list(range(len(x_axis)))
-    x_real = [i * xpixsize for i in x_i]
-    ypixsize = (max(y_axis) - min(y_axis)) / len(y_axis)
-    y_i = list(range(len(y_axis)))
-    y_real = [i * ypixsize for i in y_i]
+    x_list = list(x_axis)
+    x_diff = max(x_list) - min(x_list)
+    print()
+    x_real = np.linspace(0, x_diff, len(x_axis))
+    y_list = list(y_axis)
+    y_diff = max(y_list) - min(y_list)
+    y_real = np.linspace(0, y_diff, len(y_axis))
     return x_real, y_real
 
-def make_whole_coord(x_list, y_list):
-    new_x = [round(i) for i in x_list]
-    new_y = [round(i) for i in y_list]
-    return new_x, new_y
-
-
-def plot_XBIC(samps, c_channel):
-    for samp in samps:
-
-        for i, (c_scan, c_map) in enumerate(zip(samp['XBIC_h5s'], samp['XBIC_maps'])):
-            x_axis = c_scan['/MAPS/x_axis']  # x position of pixels  [position in um]
-            y_axis = c_scan['/MAPS/y_axis']  # y position of pixels  [position in um]
-            x, y = get_real_coordinates(x_axis, y_axis)
-            #x_r, y_r = make_whole_coord(x,y)
-            #yticks = np.linspace(0, len(y) - 1, num_ticks, dtype=np.int)
-            #yticklabels = [y[idx] for idx in yticks]
-
-            df_map = pd.DataFrame(c_map[:,:-2], index = y_r, columns = x_r[:-2])
-        plt.figure()
-        ax = sns.heatmap(df_map, square = True, xticklabels = 10).invert_yaxis()
-        plt.title(samp['Name'] + ' XBIC')
-            
-    return x_r
+def get_ticklabels(x_real, y_real):
+    # how to figure out what the proper indeices to plot are...?
+    # the integer in xticklabels represents every 'n' index to be plotted
+        # e.g. xtixklabels = 50 --> 0, 50, 150 all plotted
+        # the list x_real has actual numbers that map to the indices... but how does that help me...   
+        # could use mod?
+    # find where first whole number is in x_real!
+      # get index of this number... 
+      # when would whole number not exist...? 
+          # when the width of the axis is not evenly split by the resolution
+          # e.g. np.linspace(0,16,38)
+      # HOWEVER: the first and last number, as they are int, are guaranteed
+          # to be whole, therefore, the above approach will always find a whole number index
+          # and will plot either the bounds of the axes, or every whole number that exists in the linear space!
+    return xticks, yticks
 
 def plot_elect_maps(samps, elect_channel): # input only 'XBIC' or 'XBIV'
     if elect_channel == 'XBIC':
-        plot_XBIC(samps, elect_channel)
+        for samp in samps:
+            for c_scan, c_map, scan_num in zip(samp['XBIC_h5s'], samp['XBIC_maps'], samp['XBIC_scans']):
+                x_axis = c_scan['/MAPS/x_axis']  # x position of pixels  [position in um]
+                y_axis = c_scan['/MAPS/y_axis']  # y position of pixels  [position in um]
+                x, y = get_real_coordinates(x_axis, y_axis)
+                #xt, yt = get_ticklabels(x,y)
+                df_map = pd.DataFrame(c_map[:,:-2], index = y, columns = x[:-2])
+            print(x)
+            #plt.figure()
+            #ax = sns.heatmap(df_map, square = True, xticklabels = 50).invert_yaxis()
+            #plt.title(samp['Name'] + ' XBIC ' + str(scan_num))
     else:
-        plot_XBIV(samps, elect_channel)
+        for samp in samps:
+            for v_scan, v_map, scan_num in zip(samp['XBIV_h5s'], samp['XBIV_maps'], samp['XBIV_scans']):
+                x_axis = v_scan['/MAPS/x_axis']  # x position of pixels  [position in um]
+                y_axis = v_scan['/MAPS/y_axis']  # y position of pixels  [position in um]
+                x, y = get_real_coordinates(x_axis, y_axis)
+                df_map = pd.DataFrame(v_map[:,:-2], index = y, columns = x[:-2])
+            plt.figure()
+            ax = sns.heatmap(df_map, square = True).invert_yaxis()
+            plt.title(samp['Name'] + ' XBIV scan ' + str(scan_num))
     return
 
-# the index of the position of yticks
-# the content of labels of these yticks
+def plot_ele_maps(samps):
+    for samp in samps:
+        for scan in samp:
+            print('do ele after')
+    return
+
+
+    
