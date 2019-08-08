17b8f1d120d0cd34fcc0eac3df8a26d2c62c0b08
tzwalker <tzwalker@asu.edu>
2019-08-07 18:48:24 -0700

    hexbin plotting in quick_plots

--- python/1_NBL3_code/z_quick_plotting.py
+++ python/1_NBL3_code/z_quick_plotting.py
@@ -7,18 +7,21 @@ import matplotlib.pyplot as plt
 def plot_nice_stats(samp, scan, x, y, axis_label_sizes):
     x_data = samp['c_redStand_arrs'][scan][:,x_channel]
     y_data = samp['c_redStand_arrs'][scan][:,y_channel]
-    data_models = samp['c_kmodels'][scan] # --> XBIC, Cu, and Cd arrays for scan422
-    labels = data_models.labels_
+    #data_models = samp['c_kmodels'][scan] # --> XBIC, Cu, and Cd arrays for scan422
+    #labels = data_models.labels_
     
     fig, ax0 = plt.subplots()
-    ax0 = sns.scatterplot(x_data, y_data, s = 15, hue = labels, palette = "Set1")
-
-    # figure level
-    plt.xlabel('Stand. ' + elements_in[x-1] + ' (a.u.)', fontsize=axis_label_sizes)
-    plt.ylabel('Stand. XBIC (a.u.)', fontsize=axis_label_sizes)
-    #plt.title(samp['Name'] + ' scan ' + str(samp['XBIC_scans'][scan]))
-    # axis level
-    ax0.tick_params(labelsize = 14)                     #formats size of ticklabels
+    sns.set(font_scale = 1.5)
+    ax0 = sns.jointplot(x_data, y_data, kind='hex', color='g')
+    ax0.set_axis_labels('Stand. ' + elements_in[x-1] + ' (a.u.)', 'Stand. XBIC (a.u.)')
+# =============================================================================
+#     # figure level
+#     plt.xlabel('Stand. ' + elements_in[x-1] + ' (a.u.)', fontsize=axis_label_sizes)
+#     plt.ylabel('Stand. XBIC (a.u.)', fontsize=axis_label_sizes)
+#     #plt.title(samp['Name'] + ' scan ' + str(samp['XBIC_scans'][scan]))
+#     # axis level
+#     ax0.tick_params(labelsize = 14)                     #formats size of ticklabels
+# =============================================================================
     return #plt.show(fig)
 
 
@@ -27,7 +30,7 @@ scan = 0     # --> index of scan
 x_channel = 1 # --> 0:XBIC 1:Cu 2:Cd
 y_channel = 0 # --> 0:XBIC 1:Cu 2:Cd
 label_sizes = 16
-plot_nice_stats(samp, scan_i, x_channel, y_channel, label_sizes)
+plot_nice_stats(samp, scan, x_channel, y_channel, label_sizes)
 
 #z = test_data[:, z_variable]
 
