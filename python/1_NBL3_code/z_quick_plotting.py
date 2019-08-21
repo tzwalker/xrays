17ab3772a6ded14f024c666a44df8faccebb3614
tzwalker <tzwalker@asu.edu>
2019-08-20 18:39:19 -0700

    playing more with gradients

--- python/1_NBL3_code/z_quick_plotting.py
+++ python/1_NBL3_code/z_quick_plotting.py
@@ -3,8 +3,9 @@
     # next step: apply gaussian filter to XRF, then find edges (or alter Sobel kernels, ,if possible)
     
 import numpy as np
-import seaborn as sns
+import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.colors import colorConverter 
 from skimage import filters
 from scipy.ndimage import gaussian_filter
 
@@ -14,39 +15,50 @@ cu_map = samp_maps[0][:,:-2]
 zn_map = samp_maps[2][:,:-2]
 
 sig_filt = 1
-thres = 75
+
 # gaussian filter of original data
 cu_gauss = gaussian_filter(cu_map, sigma=sig_filt)                 # maintains shape
 zn_gauss = gaussian_filter(zn_map, sigma=sig_filt)
-
+# gradients of gaussian filtered image
 cu_gradient = filters.sobel(cu_gauss)                       # maintains shape
-cu_gradient_no_edge = cu_gradient[np.nonzero(cu_gradient)] # returns raveled array
 zn_gradient = filters.sobel(zn_gauss)
+# gradients without trimmed 'zero 'edges; prepping to find percentiles
+cu_gradient_no_edge = cu_gradient[np.nonzero(cu_gradient)] # returns raveled array
 zn_gradient_no_edge = zn_gradient[np.nonzero(zn_gradient)]
+# pix by pix product
+product = cu_gradient * zn_gradient
+# geometric mean of gradients
+gmean = np.sqrt(product)
 
-cu_gradient_75quart_thres = np.percentile(cu_gradient_no_edge, thres)
-zn_gradient_75quart_thres = np.percentile(zn_gradient_no_edge, thres)
+# generate the colors for your colormap
+color1 = colorConverter.to_rgba('c')
+color2 = colorConverter.to_rgba('c')
+# make the colormaps
+cmap1 = matplotlib.colors.LinearSegmentedColormap.from_list('my_cmap',['k','tab:orange'])
+cmap2 = matplotlib.colors.LinearSegmentedColormap.from_list('my_cmap2',[color1,color2])
+#cmap1._init() # create lut array with rgba values
+cmap2._init() # create the _lut array, with rgba values
+# create your alpha array and fill the colormap with them
+# here it is progressive, but you can create whathever you want
+#alphas1 = np.linspace(0, 0.9, cmap1.N+3)
+alphas2 = np.linspace(0, 1.0, cmap2.N+3)
+#cmap1._lut[:,1] = alphas1
+cmap2._lut[:,-1] = alphas2
+fig, axs = plt.subplots(1,3)
+plt.tight_layout()
+axs[0].imshow(cu_gradient, cmap='Oranges_r', origin='lower')
+axs[1].imshow(zn_gradient, cmap='Blues_r', origin='lower')
+axs[2].imshow(cu_gradient, cmap='Oranges_r', origin='lower')
+axs[2].imshow(zn_gradient, cmap=cmap2, origin='lower')
 
-cu_mask = np.where(cu_gradient > cu_gradient_75quart_thres, 1,0)
-zn_mask = np.where(zn_gradient > zn_gradient_75quart_thres, 1,0)
-
-# foro each pixel in the masks, if values match, record pixel coordinates for indexing into real data arrays, plot on scatter
-     # --> matches pixels in the gradient maps that are above a certaiin percentile... make bulk stats of number of matches among the various areas scanned
-
+plt.figure()
+plt.scatter(cu_gradient_no_edge,zn_gradient_no_edge, s=3, color='#6B8E23')
+plt.plot(np.unique(cu_gradient_no_edge), np.poly1d(np.polyfit(cu_gradient_no_edge, zn_gradient_no_edge, 1))(np.unique(cu_gradient_no_edge)), color='#20B2AA')
 #fig, (ax0, ax1, ax2) = plt.subplots(nrows=1,ncols=3)
 #plt.tight_layout()
-#sns.heatmap(origin_map, square=True, xticklabels=20, yticklabels=20, ax=ax0, cbar_kws={'shrink':0.5}).invert_yaxis()
-#sns.heatmap(gauss_map, square=True, xticklabels=20, yticklabels=20, ax=ax1, cbar_kws={'shrink':0.5}).invert_yaxis()
-#sns.heatmap(grad_matrix, square=True, xticklabels=20, yticklabels=20, ax=ax2, cbar_kws={'shrink':0.5}).invert_yaxis()
-plt.figure()
-sns.heatmap(cu_mask, square=True, xticklabels=20,yticklabels=20,cbar_kws={'shrink':0.5}, cmap='BrBG').invert_yaxis()
-sns.heatmap(zn_mask, square=True, xticklabels=20,yticklabels=20,cbar_kws={'shrink':0.5}, cmap='PiYG').invert_yaxis()
-#sns.regplot(cu_stand.ravel(), zn_stand.ravel(), scatter_kws={'s':5})
-#sns.jointplot(origin_map.ravel(), origin_map1.ravel(), kind='hex')
-#plt.figure()
-#sns.regplot(no_edge_gmatrix, no_edge_gmatrix1, scatter_kws={'s':5})
-#sns.jointplot(no_edge_gmatrix, no_edge_gmatrix1, kind='hex')
-
+#ax0.imshow(cu_gradient, origin='lower', cmap='Greys_r')
+#ax1.imshow(zn_gradient, origin='lower', cmap='Greys_r')
+#ax2.imshow(cu_map, origin='lower', cmap='magma')
 
 # =============================================================================
 # fig, (ax0,ax1) = plt.subplots(nrows=1,ncols=2)
@@ -68,4 +80,31 @@ sns.heatmap(zn_mask, square=True, xticklabels=20,yticklabels=20,cbar_kws={'shrin
 # gauss_of_cu_stand = gaussian_filter(cu_stand, sigma=2)
 # zn_stand = zn_stand.reshape(np.shape(gauss_map))
 # gauss_of_zn_stand = gaussian_filter(zn_stand, sigma=2)
+# =============================================================================
+
+
+
+
+# =============================================================================
+# # threshold percentile of gradient map
+# cu_gradient_75quart_thres = np.percentile(cu_gradient_no_edge, percentile)
+# zn_gradient_75quart_thres = np.percentile(zn_gradient_no_edge, percentile)
+# # find pixels in gradient map within a given percentile
+# cu_mask = np.where(cu_gradient >= cu_gradient_75quart_thres, 1,0) # retains shape
+# zn_mask = np.where(zn_gradient >= zn_gradient_75quart_thres, 1,0)
+# # find where gradient maps mismatch --> quantifies the error in saying g.bs are where the gradients lie??
+# a_mismatched_map = np.where(cu_mask == zn_mask, 1, 0)
+# # parameter to minimize mismatch
+# mismatch = a_mismatched_map.sum()
+# print(percentile, mismatch, round(mismatch/init_mismatch, 3))
+# 
+# fig, (ax0, ax1, ax2) = plt.subplots(nrows=1,ncols=3)
+# plt.tight_layout()
+# ax0.imshow(cu_mask, origin='lower', cmap='Greys_r')
+# ax1.imshow(zn_mask, origin='lower', cmap='Greys_r')
+# ax2.imshow(a_mismatched_map, origin='lower', cmap='Greys_r')
+# #if mismatch < init_mismatch:
+#     #best_percentile = percentile
+# # for each pixel in the masks, if values match, record pixel coordinates for indexing into real data arrays, plot on scatter
+#      # --> matches pixels in the gradient maps that are above a certaiin percentile... make bulk stats of number of matches among the various areas scanned
 # =============================================================================
