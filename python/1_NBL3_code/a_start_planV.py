068616d1357d8fef01c29cf9784a56235329bcdc
tzwalker <tzwalker@asu.edu>
2019-09-04 15:58:51 -0700

    clustering for indices and simple masking boxplots

--- python/1_NBL3_code/a_start_planV.py
+++ python/1_NBL3_code/a_start_planV.py
@@ -10,17 +10,17 @@ def get_directory(machine_index):
         def_path = '/home/kineticcross/Desktop/xrays/python'
     return scan_path, def_path
 
-scan_path, def_path = get_directory(1)
+scan_path, def_path = get_directory(0)
     
 import sys
 sys.path.append(def_path)
 from b_import_h5 import get_add_h5s
 from b_scale_elect import get_add_electrical
 import c_rummage_thru_H5 as rumH
-#import d_clustering
+import d_clustering
 import e_statistics
 
-NBL3_2 = {'Name': 'Med. Cu (450°C)', 'XBIC_scans': [422,423,424, 550], 'XBIV_scans': [419,420,421, 551], 
+NBL3_2 = {'Name': 'NBL3-2', 'XBIC_scans': [422,423,424, 550], 'XBIV_scans': [419,420,421, 551], 
           'beam_conv':      [2E5,2E5,2E5, 2E5], 
           'c_stanford':     [5000,5000,5000, 50000], 
           'c_lockin':       [500,500,500, 100], 
@@ -28,17 +28,17 @@ NBL3_2 = {'Name': 'Med. Cu (450°C)', 'XBIC_scans': [422,423,424, 550], 'XBIV_sc
           # wrong key decriptor 2017_12_2IDD, but geometry same between the two beamtimes
           '2017_12_ele_iios': [0.275, 0.0446, 0.0550], 
           '2019_03_ele_iios': [0.104, 0.00131, 0.00418]}
-NBL3_3 = {'Name': 'Hi. Cu, (450°C)', 'XBIC_scans': [264,265,266, 491], 'XBIV_scans': [261,262,263, 472], 
+NBL3_3 = {'Name': 'NBL3_3', 'XBIC_scans': [264,265,266, 491], 'XBIV_scans': [261,262,263, 472], 
           'beam_conv':      [2E5, 2E5, 2E5, 1E5], 
           'c_stanford':     [5000,5000,5000, 200], 
           'c_lockin':       [500,500,500, 20], 
           'v_lockin':       [1E4,1E4,1E4, 100000],
           '2017_12_ele_iios': [0.296, 0.0488, 0.0604],
           '2019_03_ele_iios': [0.114, 0.00144, 0.00459]}
-TS58A = {'Name': 'Med. Cu (420°C)', 'XBIC_scans': [385,386,387, 439], 'XBIV_scans': [382,383,384, 440], 
+TS58A = {'Name': 'TS58A', 'XBIC_scans': [385,386,387, 439], 'XBIV_scans': [382,383,384, 440], 
          'beam_conv':       [2E5, 2E5, 2E5, 1E5], 
          'c_stanford':      [5000,5000,5000, 200], 
-         'c_lockin':        [500,10000,10000, 20], 
+         'c_lockin':        [10000,10000,10000, 20], # the scaler values recordered were 10000, however, better comparison can be made with the 500 values seen here...
          'v_lockin':        [1000,1000,1000, 100000],
          '2017_12_ele_iios': [0.381, 0.0682, 0.0867],
          '2019_03_ele_iios': [0.162, 0.00209, 0.00669]}
@@ -72,23 +72,25 @@ e_statistics.standardize_channels(samples,
     # maps according to bad data in one of the XRF channels
     # not configured for using electrical channels as the bad channel
     # see ReadMe.txt for details
-#e_statistics.reduce_arrs(samples, 'Cu', elements, 3,                    # int = # of std
-                         #['c_stat_arrs', 'v_stat_arrs'],                # reference data
-                         #['c_reduced_arrs', 'v_reduced_arrs'])          # new data
+e_statistics.reduce_arrs(samples, 'Cu', elements, 3,                    # int = # of std
+                         ['c_stat_arrs', 'v_stat_arrs'],                # reference data
+                         ['c_reduced_arrs', 'v_reduced_arrs'])          # new data
 
 
-#e_statistics.standardize_channels(samples, ['c_reduced_arrs', 'v_reduced_arrs'], ['c_redStand_arrs', 'v_redStand_arrs'])
+e_statistics.standardize_channels(samples, 
+                                  ['c_reduced_arrs', 'v_reduced_arrs'], 
+                                  ['c_redStand_arrs', 'v_redStand_arrs'])
 
 # 'perf' is electrical: will be performed for both XBIC and XBIV if entered
     # type 'all' to include all features, that is, the electrical channel and
     # all elements in 'elements_in'
-#cluster_channels = ['perf', 'Cu'] 
-#cluster_number = 3
+cluster_channels = ['perf'] 
+cluster_number = 3
 # the integer argument in this function is a switch that deetermiens which data to cluster
 # 0 --> original data (no NaN), 1 --> standardized data, 
 # 2 --> reduced original data, 3 --> reduced standardized data
 # switches 1 and 3 are reccomended as they use the standardized data, switch 3 recommended for scatter plots
-#d_clustering.kclustering(samples, cluster_number, cluster_channels, elements_in, 1)
+d_clustering.kclustering(samples, cluster_number, cluster_channels, elements, 2)
 
 # use separate programs for plotting
 
--- python/1_NBL3_code/simple_masking.py
+++ python/1_NBL3_code/simple_masking.py
@@ -6,29 +6,108 @@ Mon Aug 26 17:49:23 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-scan = 2
+#samp = TS58A
+scans = [0,1,2,3]
+model = 'c_kmodels'
+data = 'c_reduced_arrs'
+clusts = [0,1,2]
 
-cu_maps = [samp['elXBIC_corr'][scan][0][:,:-2] for samp in samples]
-xbic_maps = [samp['XBIC_maps'][scan][:,:-2] for samp in samples]
-
-cu_data = [samp['elXBIC_corr'][scan][0][:,:-2].ravel() for samp in samples]
-xbic_data = [samp['XBIC_maps'][scan][:,:-2].ravel() for samp in samples]
+def plot_clust_boxes(clust_nums, clust_labs, x, y, z, scan, sam):
+    fig, axs = plt.subplots(3,1)
+    plt.tight_layout()
+    x1 = [x[np.where(clust_labs == clust)[0]] for clust in clust_nums]
+    y1 = [y[np.where(clust_labs == clust)[0]] for clust in clust_nums]
+    z1 = [z[np.where(clust_labs == clust)[0]] for clust in clust_nums]
     
-x_boxlabs = [samp['Name'] for samp in samples]
+    bp_dictx = axs[0].boxplot(x1, showfliers=False)
+    for line in bp_dictx['medians']:
+        med = line.get_ydata() # get median value array (horizontal line)
+        xpoint, ypoint = line.get_xydata()[1] # get plot coordinates of median
+        # annotate this position with median as string
+        axs[0].annotate(xy=(xpoint,ypoint), 
+           s=' ' + "{:.3f}".format(med[0]), 
+           horizontalalignment='left') # draw above, centered
+    axs[0].title.set_text('reduced copper')
+    
+    bp_dicty = axs[1].boxplot(y1, showfliers=False)
+    for line in bp_dicty['medians']:
+        med = line.get_ydata()
+        # get position data for median line
+        xpoint, ypoint = line.get_xydata()[1] # top of median line
+        # overlay median value
+        axs[1].annotate(xy=(xpoint,ypoint), 
+           s=' ' + "{:.2e}".format(med[0]), horizontalalignment='left')
+    axs[1].title.set_text('reduced xbic')
+    
+    bp_dictz = axs[2].boxplot(z1, showfliers=False)
+    for line in bp_dictz['medians']:
+        med = line.get_ydata()
+        # get position data for median line
+        xpoint, ypoint = line.get_xydata()[1] # top of median line
+        # overlay median value
+        axs[2].annotate(xy=(xpoint,ypoint), 
+           s=' ' + "{:.2e}".format(med[0]), horizontalalignment='left')
+    axs[2].title.set_text('reduced cd')
+    plt.savefig(r'C:\Users\Trumann\Desktop\Plot Directory\NBL3\20190904 clustering and boxplots\{sample}_area{sc}'.format(sample=sam, sc=scan))
+    return
 
-fig, axs = plt.subplots(1,2)
-plt.tight_layout()
-axs[0].imshow(cu_maps[2], origin='lower')
-axs[1].imshow(xbic_maps[2], origin='lower')
+def plot_more_boxes(model, data, scans):
+    for samp, samp_name in zip(samples, samp_names):
+        for scan in scans:
+            clust_labs = samp[model][scan].labels_
+            #clust_cent  = samp[model][scan].cluster_centers_
+            data_xbic = samp[data][scan][:,0]
+            data_cu = samp[data][scan][:,1]
+            data_cd = samp[data][scan][:,2]
+            plot_clust_boxes(clusts, clust_labs, data_cu, data_xbic, data_cd, scan, samp_name)
+    return
+samp_names = ['NBL3_2', 'NBL3_3', 'TS58A']
+plot_more_boxes(model, data, scans)
 
-plt.figure()
-plt.boxplot(cu_data)
-plt.xticks([1, 2, 3], x_boxlabs)
-plt.suptitle('Thickness corrected Cu')
-plt.figure()
-plt.boxplot(xbic_data)
-plt.xticks([1, 2, 3], x_boxlabs)
-plt.suptitle('XBIC')
+# =============================================================================
+# def print_clust_means(clust_nums, x, y):
+#     x1 = [x[np.where(clust_labs == clust)[0]] for clust in clust_nums]
+#     y1 = [y[np.where(clust_labs == clust)[0]] for clust in clust_nums]
+#     for i, (xclust, yclust) in enumerate(zip(x1,y1)):
+#         print('copper clust ' + str(i) + ': ' + str(np.mean(xclust)))
+#         print('xbic clust ' + str(i) + ': ' + str(np.mean(yclust)))
+#     return
+#     
+# #print_clust_means(clust, data_cu, data_xbic)
+# =============================================================================
+# =============================================================================
+#     for clust in clust_nums:
+#         
+#         indices = np.where(clust_labs == clust)[0]
+#         x1 = x[indices]
+#         y1 = y[indices]
+# =============================================================================
+
+### basic boxplots of raw data 
+# =============================================================================
+# 
+# cu_maps = [samp['elXBIC_corr'][scan][0][:,:-2] for samp in samples]
+# xbic_maps = [samp['XBIC_maps'][scan][:,:-2] for samp in samples]
+# 
+# cu_data = [samp['elXBIC_corr'][scan][0][:,:-2].ravel() for samp in samples]
+# xbic_data = [samp['XBIC_maps'][scan][:,:-2].ravel() for samp in samples]
+#     
+# x_boxlabs = [samp['Name'] for samp in samples]
+# 
+# fig, axs = plt.subplots(1,2)
+# plt.tight_layout()
+# axs[0].imshow(cu_maps[2], origin='lower')
+# axs[1].imshow(xbic_maps[2], origin='lower')
+# 
+# plt.figure()
+# plt.boxplot(cu_data)
+# plt.xticks([1, 2, 3], x_boxlabs)
+# plt.suptitle('Thickness corrected Cu')
+# plt.figure()
+# plt.boxplot(xbic_data)
+# plt.xticks([1, 2, 3], x_boxlabs)
+# plt.suptitle('XBIC')
+# =============================================================================
 
 ### plotting and correlating (via scatter) gaussian filtered standardized arrays
 # =============================================================================
