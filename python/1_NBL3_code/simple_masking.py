fe5927cf48b3f77b9354a2655407f815ee885592
tzwalker <tzwalker@asu.edu>
2019-09-09 09:43:18 -0700

    pearson coeff matrices made

--- python/1_NBL3_code/simple_masking.py
+++ python/1_NBL3_code/simple_masking.py
@@ -7,6 +7,84 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def get_cluster_data(samp, scan, model_key, data_key, num_of_clust):
+    data = samp[data_key][scan]
+    clust_labs = samp[model_key][scan].labels_
+    clust_nums = list(range(num_of_clust))
+    cluster_data = [data[np.where(clust_labs == clust)[0]] for clust in clust_nums]
+    return cluster_data
+
+def high_and_low_corrcoeffs(medians_of_each_cluster, whole_cluster_data, pertinent_channel):
+    electrical_medians = medians_of_each_cluster[:,pertinent_channel] # if pertinent_channel=0 --> gets xbic medians; they are first column in medians_of_each_cluster
+    high_cluster, low_cluster = np.argmax(electrical_medians), np.argmin(electrical_medians) # returns indices (i.e. cluster) of highest and lowest median xbic
+    high_x_corr = np.corrcoef(whole_cluster_data[high_cluster].T) # generate the correlation matrices for th best and worst cluster
+    low_x_corr = np.corrcoef(whole_cluster_data[low_cluster].T)
+    # essentially, use the median array to index-into/identify the desired cluster; in this case, best and worst electrical clusters
+    return high_x_corr, low_x_corr
+
+def high_xbic_cluster_correlations(samp, scans, model, data_key, cluster_number, chan_of_interest):
+    high_xbics = []
+    low_xbics = []
+    for scan in scans:
+        cluster_data = get_cluster_data(samp, scan, model, data_key, cluster_number)
+        medians = [np.median(cluster, axis=0) for cluster in cluster_data] # returns median of value of each feature for each cluster
+        medians_arr = np.array(medians) # arrays the above data for convenience    
+        high = high_and_low_corrcoeffs(medians_arr, cluster_data, chan_of_interest)[0]
+        low = high_and_low_corrcoeffs(medians_arr, cluster_data, chan_of_interest)[1]
+        high_xbics.append(high)
+        low_xbics.append(low)
+    high_xbics = np.array(high_xbics)
+    low_xbics = np.array(low_xbics)
+    return high_xbics, low_xbics
+
+samp = NBL3_2
+scans = 0 #[0,1,2,3]
+model = 'c_kmodels'
+data_key = 'c_redStand_arrs'
+channel_of_interest = 0 # --> index of channel median (xbic,cu,cd, etc...) you'd like to use for identifying cluster of interest (not sure if necessary)
+
+# returns pearson correlation coeff matrix of all loaded elements and electrical channel
+    # for the highest performing clusters in each map according to xbic median value
+#high_xbic_corrs = high_xbic_cluster_correlations(samp, scans, model, data_key, cluster_number, channel_of_interest)[0]
+#low_xbic_corrs = high_xbic_cluster_correlations(samp, scans, model, data_key, cluster_number, channel_of_interest)[1]
+## want to revise so high and low clusters may be specfied beforehand 
+    # do not want to calculate both at once
+    
+# make combination counter
+def return_cluster_unique_lists(cluster_data):
+    medians = [np.median(cluster, axis=0) for cluster in cluster_data]
+    arr_medians = np.array(medians).T # for convenience of max/min identification between clusters
+    
+    indices_of_median_maxes = []
+    indices_of_median_mins = []
+    for channel in medians:
+        index_of_median_maxes = channel.index(max(channel)) #--> really "cluster containing median max"
+        index_of_median_mins = channel.index(min(channel))  #--> really "cluster containing median min"
+        indices_of_median_maxes.append(index_of_median_maxes)
+        indices_of_median_mins.append(index_of_median_mins)
+    
+    # these lists trade off, producing a unique identifier for a given combo in the max XBIC cluster
+    indices_test_cluster0 = [i for i, cluster in enumerate(indices_of_median_maxes) if cluster == 0]
+    indices_test_cluster1 = [i for i, cluster in enumerate(indices_of_median_maxes) if cluster == 1]
+    indices_test_cluster2 = [i for i, cluster in enumerate(indices_of_median_maxes) if cluster == 2]
+    return 
+# =============================================================================
+# z_cluster_data = get_cluster_data(samp, scan, model, data_key, cluster_number)
+# z_test_medians = [np.median(cluster, axis=0) for cluster in z_cluster_data]
+# z_test_medians_arr = np.array(z_test_medians)
+# z_test_medians_max = np.max(z_test_medians_arr, axis=0) # need to make tuple (index of feature, index of feature)
+# =============================================================================
+
+
+
+### ####
+def plot_more_boxes(model, key, scans):
+    for samp, samp_name in zip(samples, samp_names):
+        for scan in scans:
+            clust_labs = samp[model][scan].labels_
+            data = [samp[key][scan][:,i] for i, ele in enumerate(samp[key][scan].T)] # this format is necessary for boxplots
+            plot_clust_boxes(clusts, clust_labs, data, scan, samp_name)
+    return
 
 def plot_clust_boxes(clust_nums, clust_labs, data, scan, sam):
     fig, axs = plt.subplots(len(data),1)
@@ -25,63 +103,20 @@ def plot_clust_boxes(clust_nums, clust_labs, data, scan, sam):
                s=' ' + "{:.4g}".format(med[0]), 
                horizontalalignment='left') # add text with formatting
         axs[i].title.set_text(boxplot_names[i])
-    plt.savefig(r'C:\Users\Trumann\Desktop\Plot Directory\NBL3\20190904 clustering and boxplots\noflier_{sample}_area{sc}'.format(sample=sam, sc=scan))
+    #plt.savefig(r'C:\Users\Trumann\Desktop\Plot Directory\NBL3\20190904 clustering and boxplots\noflier_{sample}_area{sc}'.format(sample=sam, sc=scan))
     return
 
-def get_cluster_data(samp, scan, model_key, data_key, num_of_clust):
-    data = samp[data_key][scan]
-    clust_labs = samp[model_key][scan].labels_
-    clust_nums = list(range(num_of_clust))
-    cluster_data = [data[np.where(clust_labs == clust)[0]] for clust in clust_nums]
-    return cluster_data
+#samp_names = ['NBL3_2', 'NBL3_3', 'TS58A']
+#boxplot_names = ['reduced xbic', 'reduced cu','reduced cd','reduced te']
+#plot_more_boxes(model, data_key, scans)
 
-samp = NBL3_2
-scan = 0
-model = 'c_kmodels'
-data_key = 'c_reduced_arrs'
 
-z_cluster_data = get_cluster_data(samp, scan, model, data_key, cluster_number)
 
 def clust_scatter_matrix(cluster_data, elements):
     cols = elements.insert(0, 'xbic')
     df = pd.DataFrame(cluster_data[0], columns = cols) # make for loop to go over each cluster in cluster_data
     pd.plotting.scatter_matrix(df, alpha = 0.2)
     return
-
-z_test_corr = [np.corrcoef(cluster_data.T) for cluster_data in z_cluster_data]
-z_test_medians = [np.median(cluster, axis=0) for cluster in z_cluster_data]
-z_test_arr_medians = np.array(z_test_medians)
-max_list = np.max(z_test_arr_medians, axis=0) # need to make tuple (index of feature, index of feature)
-
-### ####
-def plot_more_boxes(model, key, scans):
-    for samp, samp_name in zip(samples, samp_names):
-        for scan in scans:
-            clust_labs = samp[model][scan].labels_
-            data = [samp[key][scan][:,i] for i, ele in enumerate(samp[key][scan].T)] #this format is necessary for boxplots
-            plot_clust_boxes(clusts, clust_labs, data, scan, samp_name)
-    return
-#samp_names = ['NBL3_2', 'NBL3_3', 'TS58A']
-#boxplot_names = ['reduced xbic', 'reduced cu','reduced cd','reduced te']
-#plot_more_boxes(model, data_key, scans)
-
-def return_cluster_unique_lists(cluster_data):
-    medians = [np.median(cluster, axis=0) for cluster in cluster_data]
-    arr_medians = np.array(medians).T # for convenience of max/min identification between clusters
-    
-    indices_of_median_maxes = []
-    indices_of_median_mins = []
-    for channel in medians:
-        index_of_median_maxes = channel.index(max(channel)) #--> really "cluster containing median max"
-        index_of_median_mins = channel.index(min(channel))  #--> really "cluster containing median min"
-        indices_of_median_maxes.append(index_of_median_maxes)
-        indices_of_median_mins.append(index_of_median_mins)
-    
-    # these lists trade off, producing a unique identifier for a given combo in the max XBIC cluster
-    indices_test_cluster0 = [i for i, cluster in enumerate(indices_of_median_maxes) if cluster == 0]
-    indices_test_cluster1 = [i for i, cluster in enumerate(indices_of_median_maxes) if cluster == 1]
-    indices_test_cluster2 = [i for i, cluster in enumerate(indices_of_median_maxes) if cluster == 2]
-    return 
 
 # =============================================================================
 # test_combo_labels = list(range(len(elements)+1)) #--> change these to the indices 0,1,2,3 ; or however many channels you have
