83295b738bec495b09acedfb4ded4f8f7f321909
tzwalker <tzwalker@asu.edu>
2019-10-02 10:26:14 -0700

    refined iio sim

--- python/iio_vs_thickness_simulation.py
+++ python/iio_vs_thickness_simulation.py
@@ -44,17 +44,41 @@ def beamIn_vs_depth(thickness_increments, dt):
         iio_cdte[index] = iio_in * np.exp(beam_in)
     return iio_cdte
 
+def generate_deviated_thicknesses(roughness_deviations):
+    rough_ups = []
+    rough_downs = []
+    for roughness in roughness_deviations:
+        rough_up = no_rough * (1+roughness)
+        rough_down = no_rough * (1-roughness)
+        rough_ups.append(rough_up)
+        rough_downs.append(rough_down)
+    return rough_ups, rough_downs
+
+def rough_iios(rough_ups, rough_downs):
+    ele_rough_iios_up =     [iio_vs_depth(ele, roughness, dt) for roughness in rough_ups]
+    ele_rough_iios_up = np.stack(ele_rough_iios_up, axis=1)
+    ele_rough_iios_down =   [iio_vs_depth(ele, roughness, dt) for roughness in rough_downs]
+    ele_rough_iios_down = np.stack(ele_rough_iios_down, axis=1)
+    return ele_rough_iios_up, ele_rough_iios_down
+
 ## define settings and stack parameters ##
-beam_energy = 8.99 #keV
-beam_theta = 90                                                     #angle of the beam relative to sample normal
+beam_time = 'good_geom'
+if beam_time == 'good_geom':
+    beam_energy = 8.99
+    beam_theta = 90
+    detect_theta = 43
+elif beam_time == 'bad_geom':
+    beam_energy = 12.7
+    beam_theta = 75
+    detect_theta = 15
+
 beam_geometry = np.sin(beam_theta*np.pi/180)                        #convert to radians
-detect_theta = 43                                                   #angle of the detector sample normal
 detect_geometry = np.sin(detect_theta*np.pi/180)                    #convert to radians
 # enter thickness of layer in cm
 # only upstream layers are of interest
-MO =    {'Thick':0.00005,    'LDensity': 10.2, 'Name': 'Mo',     'capXsect': xl.CS_Total_CP('Mo', beam_energy)}
-ZNTE =  {'Thick':0.0000375,  'LDensity': 6.34, 'Name': 'ZnTe',   'capXsect': xl.CS_Total_CP('ZnTe', beam_energy)}
-CDTE =  {'Thick':0.0012000,  'LDensity': 5.85, 'Name': 'CdTe',   'capXsect': xl.CS_Total_CP('CdTe', beam_energy)}
+MO =    {'Thick':0.00005,    'LDensity': 10.2, 'capXsect': xl.CS_Total_CP('Mo', beam_energy)}
+ZNTE =  {'Thick':0.0000375,  'LDensity': 6.34, 'capXsect': xl.CS_Total_CP('ZnTe', beam_energy)}
+CDTE =  {'Thick':0.0012000,  'LDensity': 5.85, 'capXsect': xl.CS_Total_CP('CdTe', beam_energy)}
 
 # enter element for which you wish to see I/Io (will work on generatign plots for many elements at once)
 ele = 'Cu'
@@ -68,48 +92,30 @@ beam_attn = beamIn_vs_depth(no_rough, dt)
 beam_attn = beam_attn.reshape(-1,1)
 
 ## calc XRF reabsorption reference profile
-no_rough_iio = iio_vs_depth(ele, no_rough, dt) 
-no_rough_iio = no_rough_iio.reshape(-1,1)   # format array      
+ref_iio = iio_vs_depth(ele, no_rough, dt) 
+ref_iio = ref_iio.reshape(-1,1)   # format array      
+
+## calc roughness profiles
+# specfiy roughness parameters
+deviations = np.linspace(0.05, 0.2, 3)
+rough_ups, rough_downs = generate_deviated_thicknesses(deviations)
+rough_up, rough_down = rough_iios(rough_ups, rough_downs)
 
 plt.plot(x_for_plotting, beam_attn)
+plt.plot(x_for_plotting, ref_iio)
+plt.plot(x_for_plotting, rough_up[:,1])
+plt.plot(x_for_plotting, rough_down[:,1])
+plt.grid()
+plt.ylabel('% attenuation')
+plt.xlabel('CdTe thickness')
 
 ## save x and y for plotting in Origin
-arr_for_plotting0 = np.concatenate((x_for_plotting, beam_attn), axis=1)
-arr_for_plotting1 = np.concatenate((x_for_plotting, no_rough_iio), axis=1)
+arr_for_plotting0 = np.concatenate((x_for_plotting, beam_attn, ref_iio, 
+                                    # first column for 12.5% roughness (from 'deviations')
+                                    rough_up[:,1].reshape(-1,1), 
+                                    rough_down[:,1].reshape(-1,1)), axis=1)
 
-
-#np.savetxt(r'C:\Users\Trumann\Dropbox (ASU)\1_NBL3 data\iio_sim_' + str(detect_theta) +'deg_roughDOWN.csv', ele_rough_iios_down, delimiter=',')
-
-### calc roughness profiles
-# =============================================================================
-# # specfiy roughness parameters
-# roughnesses = np.linspace(0.05, 0.2, 3)
-# no_rough_in_um = no_rough / 1000        # convert x axis to um
-# 
-# def gen_upd_and_downs(r):
-#     rough_ups = []
-#     rough_downs = []
-#     for roughness in roughnesses:
-#         rough_up = no_rough * (1+roughness)
-#         rough_down = no_rough * (1-roughness)
-#         rough_ups.append(rough_up)
-#         rough_downs.append(rough_down)
-#     return rough_ups, rough_downs
-# 
-# def gen_up_down_iios(rough_ups, rough_downs):
-#     ele_rough_iios_up =     [iio_vs_depth(ele, roughness, dt) for roughness in rough_ups]
-#     ele_rough_iios_up = np.stack(ele_rough_iios_up, axis=1)
-#     ele_rough_iios_down =   [iio_vs_depth(ele, roughness, dt) for roughness in rough_downs]
-#     ele_rough_iios_down = np.stack(ele_rough_iios_down, axis=1)
-#     return ele_rough_iios_up, ele_rough_iios_down
-# 
-# #rough_ups, rough_downs = gen_upd_and_downs(roughnesses)
-# #ele_rough_iios_up, ele_rough_iios_down = gen_up_down_iios(rough_ups, rough_downs)
-# 
-# np.savetxt(r'C:\Users\Trumann\Dropbox (ASU)\1_NBL3 data\iio_sim_' + str(detect_theta) +'deg_beamIN.csv', ele_rough_iios_up, delimiter=',')
-# #np.savetxt(r'C:\Users\Trumann\Dropbox (ASU)\1_NBL3 data\iio_sim_' + str(detect_theta) +'deg_roughDOWN.csv', ele_rough_iios_down, delimiter=',')
-# #np.savetxt(r'C:\Users\triton\iio_sim_' + str(detect_theta) +'deg.csv', save_arr, delimiter=',')
-# =============================================================================
+np.savetxt(r'C:\Users\Trumann\Dropbox (ASU)\1_NBL3 data\for Origin iio_sims\iio_sim_' + str(detect_theta) +'deg_ALL'+ ele +'.csv', arr_for_plotting0, delimiter=',')
 
 # iio vs. depth() is essentially the same as the calculation seen in absorb_matlib_v_xraylib
 # only dependency iio_vs_depth() has on CdTe thickness is seen in the "no_rough" variable
