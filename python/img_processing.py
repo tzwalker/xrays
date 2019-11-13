4dacefeb88fc060cb4b67fcb7cf2c07496622483
tzwalker <tzwalker@asu.edu>
2019-11-13 06:34:10 -0700

    on work laptop: push w/o pull from remote

--- python/img_processing.py
+++ python/img_processing.py
@@ -12,7 +12,18 @@ io.imshow(boundaries)
 #nan_boundaries = boundaries.copy()
 #nan_boundaries[nan_boundaries==0] = np.nan
 #io.imshow(nan_boundaries)
-
+#%%
+### from interent; see py seg techniques in bookmarks
+pic = plt.imread('1.jpeg')/255  # dividing by 255 to bring the pixel values between 0 and 1
+print(pic.shape)
+plt.imshow(pic)
+pic_n = pic.reshape(pic.shape[0]*pic.shape[1], pic.shape[2])
+pic_n.shape
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=5, random_state=0).fit(pic_n)
+pic2show = kmeans.cluster_centers_[kmeans.labels_]
+cluster_pic = pic2show.reshape(pic.shape[0], pic.shape[1], pic.shape[2])
+plt.imshow(cluster_pic)
 #%%
 import matplotlib.pyplot as plt
 from skimage.filters import sobel
