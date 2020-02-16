142edd68633ec8a5a4c5ad44e290a6699d780104
gr4yh4t <gr4yh4t@protonmail.com>
2020-02-16 16:23:29 -0700

    plot sample class data

--- python/a_class.py
+++ python/a_class.py
@@ -9,67 +9,7 @@ these functions import electrical and XRF data from two types of files:
 each sample object has data related to that sample
 """
 
-import h5py
-import pandas as pd
-import numpy as np
-
-class Sample():
-    def __init__(self):
-        self.scans = []; self.h5data = []; self.lockin = []; self.maps = []
-        self.stack = {}
-    def import_scan_data(self, data_path):
-        str_list = [str(scan_number) for scan_number in self.scans]
-        filenames = [data_path+'/2idd_0'+scan+'.h5' for scan in str_list]
-        self.h5data = [h5py.File(file, 'r') for file in filenames]
-    def get_lockin(self, data_path):
-        data = pd.read_csv(data_path)
-        for scan in self.scans:
-            # get lockin data for a specific scan
-            # if 'index out of bounds error', check if scan in datafile
-            settings = data.loc[data['scan']==scan]
-            # check if scan is XBIC or XBIV
-            bic_or_biv = settings['XBIC/V'].values[0]
-            # then calc factor (cts-->ampere) for XBIC channel in h5
-            if bic_or_biv == 'C':
-                stanford = settings['stanford'].values[0]
-                V2F = settings['V2F'].values[0]
-                lockin = settings['lockin'].values[0]
-                scaler_factor = (stanford*1E-9) / (V2F*lockin)
-            elif bic_or_biv == 'V':
-                V2F = settings['V2F'].values[0]
-                lockin = settings['lockin'].values[0]
-                scaler_factor = 1 / (V2F*lockin)
-            self.lockin.append(scaler_factor)
-    def import_maps(self, eh_scaler, elements, norm_scaler, fit_access_key):
-        # order of scaler channels in h5: '/MAPS/scalers'
-        scaler_chs = ['SRCurrent', 'us_ic', 'ds_ic']
-        # to find electrical map, set scaler index
-        scaler_idx = scaler_chs.index(eh_scaler)
-        # to find channel to normalize, set normalization index
-        norm_idx = scaler_chs.index(norm_scaler)
-        # set-up to find correct normalization scalers
-        if fit_access_key == 'roi':
-            fit_keys = ['/MAPS/XRF_roi', '/MAPS/XRF_roi_quant']
-        elif fit_access_key == 'fit':  
-            fit_keys = ['/MAPS/XRF_fits','/MAPS/XRF_fits_quant']
-        # for each scan, convert and add electrical and element maps
-        for h5, factor in zip(self.h5data, self.lockin):
-            maps_for_scan = []
-            # to find element, decode h5 element strings
-            dcoded_chs = [ele_str.decode('utf-8') for ele_str in h5['/MAPS/channel_names']]
-            electrical_map = h5['/MAPS/scalers'][scaler_idx]
-            electrical_map = electrical_map * factor
-            maps_for_scan.append(electrical_map)
-            for element in elements:
-                ele_idx = dcoded_chs.index(element)
-                ele_map = h5[fit_keys[0]][ele_idx,:,:]
-                nrmlize_map = h5['/MAPS/scalers'][norm_idx, :, :] 
-                quant_map = h5[fit_keys[1]][norm_idx, 0, ele_idx]
-                fit_map = ele_map / nrmlize_map / quant_map # --> fitted map
-                maps_for_scan.append(fit_map)
-            maps_ = np.array(maps_for_scan)
-            self.maps.append(maps_)
-            
+from class_Sample import Sample
             
 data_path = '/home/kineticcross/Desktop/data'
 # create sample objects
@@ -127,4 +67,6 @@ TS58A.import_maps('ds_ic', elements, 'us_ic', 'fit')
 #   "NBL32.maps[2][0,:,:]" --> electrical map for scan 424
 #   "NBL32.maps[2][1,:,:]" --> Cu map for scan 424
 
-# clean up plotting functions... and learn how to import the class as a module
+# apply iio correction to XRF at different beamtimes
+
+# clean up plotting functions... and 
---
+++ python/definitions plotting 2.py
@@ -0,0 +1,86 @@
+"""
+notes on creating color bar onject
+    #fig.colorbar(ax0).ax0 <--> plt.gcf().axes[-1]
+    # if im = ax.ishow():
+    # cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+        # cbar.ax.set_ylabel: --> ax object needs to be accessed
+    # if ax = sns.heatmap():
+    # cbar = plt.gcf().axes[-1]
+        # cbar.set_ylabel: no ax needed, ax object already accessed
+"""
+
+# cleaning custom 2d plots
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.ticker as tkr
+import matplotlib.pyplot as plt
+
+def get_real_coordinates(h5axis):
+    data_coord = list(h5axis)
+    axis_width = max(data_coord) - min(data_coord)
+    axis_resolution = np.linspace(0, axis_width, len(data_coord))
+    round_steps = [round(i,3) for i in axis_resolution]
+    return round_steps
+
+def custom_format_ticks(axes_object_labels, string_type):
+    txt_labs = [label.get_text() for label in axes_object_labels]
+    ticking = [string_type.format(float(txt)) for txt in txt_labs]
+    return ticking
+
+def get_colorbar_axis(data, num_of_std):
+    M = data[:,:-2] # map without nan columns
+    heatmap_min = M.mean() - M.std() * num_of_std
+    heatmap_max = M.mean() + M.std() * num_of_std
+    if heatmap_min < 0:
+        heatmap_min = 0
+    return heatmap_min, heatmap_max
+
+def plot_2D_data(data, h5data, user_cmap, 
+                 cbarlabel, cbar_lblsize, cbarpad, cbar_ticklbl_size,
+                 xy_lbl_sizes, xy_ticklbl_sizes, xy_intervals, 
+                 sci_notation, ax=None,
+                 cbar_kw={}, **kwargs):
+    
+    xreal = get_real_coordinates(h5data['/MAPS/x_axis'])
+    yreal = get_real_coordinates(h5data['/MAPS/y_axis'])
+    df_map = pd.DataFrame(data, index = yreal, columns = xreal)
+    
+    formatter = tkr.ScalarFormatter(useMathText=True)
+    formatter.set_scientific(sci_notation)
+    formatter.set_powerlimits((-2, 2))
+    
+    fig, ax = plt.subplots(1)
+    # Plot the heatmap
+    sns.heatmap(df_map, square=True, cmap=user_cmap, ax=ax,
+                     xticklabels=xy_intervals[0], yticklabels=xy_intervals[1],
+                     cbar_kws={'format': formatter})
+    ax.tick_params(labelsize=xy_ticklbl_sizes)
+    #formats tick label strings without ".0"
+    x_labls = custom_format_ticks(ax.get_xticklabels(), '{:g}')
+    y_labls = custom_format_ticks(ax.get_yticklabels(), '{:g}')  
+    # replace xy ticklabels
+    ax.set_xticklabels(x_labls)
+    # invert, replace, and rotate y ticklabels
+    y_labls.reverse(); ax.set_yticklabels(y_labls, rotation = 0)  
+    # make xy axis titles (labels)
+    plt.xlabel('X (\u03BCm)', fontsize=xy_lbl_sizes)
+    plt.ylabel('Y (\u03BCm)', fontsize=xy_lbl_sizes)        
+    # Create/format colorbar
+    cbar = plt.gcf().axes[-1] # colorbar object in seaborn heatmap
+    cbar.set_ylabel(cbarlabel, rotation=90, va="bottom", 
+                       size=cbar_lblsize, labelpad=cbarpad)
+    cbar.tick_params(labelsize=cbar_ticklbl_size)   
+    # change size of color bar scale label, e.g. 1e-8
+    cbar.yaxis.get_offset_text().set(size=cbar_ticklbl_size)
+    # shift position of color bar scale label      
+    cbar.yaxis.set_offset_position('left')
+    return
+#ex = np.random.random((101,101))
+scan_idx = 5
+plot_2D_data(NBL33.maps[scan_idx][1,:,:], NBL33.h5data[scan_idx], 
+             user_cmap='Oranges_r',
+             xy_intervals=[20,20], xy_ticklbl_sizes=14, xy_lbl_sizes=16,
+             cbarlabel="$\u03BCg/cm{^2}$", cbar_lblsize=14, cbarpad=25, 
+             cbar_ticklbl_size=12,
+             sci_notation=True)
