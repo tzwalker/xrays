7b0c0fdd6521dd5e63d715bb6342337a6133c172
tzwalker <tzwalker@asu.edu>
2020-03-04 08:49:29 -0700

    included NBL31

--- python/class home.py
+++ python/class home.py
@@ -13,11 +13,19 @@ from class_Sample import Sample
 import class_absorb_correct as XRFcorr
             
  #'/home/kineticcross/Desktop/data'
-data_path = r'C:\Users\Trumann\Desktop\NBL3_data\all_H5s' 
+data_path = r'C:\Users\triton\Desktop\NBL3_data\plan_view' #r'C:\Users\Trumann\Desktop\NBL3_data\all_H5s' 
 # create sample objects
-NBL32 = Sample(); NBL33 = Sample(); TS58A = Sample()
+NBL31 = Sample(); NBL32 = Sample(); NBL33 = Sample(); TS58A = Sample()
 
 # define stack and scans of each sample
+NBL31.stack = {'Mo':   [10.2, 500E-7], 
+                 'ZnTe': [6.34, 375E-7], 
+                 'Cu':   [8.96, 0.5E-7], 
+                 'CdTe': [5.85, 10.85E-4], 
+                 'CdS':  [4.82, 80E-7], 
+                 'SnO2': [100E-7]}
+NBL31.scans = [335,336,337, 338,339,340, 341,342,343,344, 517,519]
+
 NBL32.stack = {'Mo':   [10.2, 500E-7], 
                  'ZnTe': [6.34, 375E-7], 
                  'Cu':   [8.96, 2.5E-7], 
@@ -43,12 +51,14 @@ TS58A.stack = {'Mo':   [10.2, 500E-7],
 TS58A.scans =  [386, 439, 385,387, 382,383,384, 408,427,440]
 
 # import h5 data for each sample
+NBL31.import_scan_data(data_path)
 NBL32.import_scan_data(data_path)
 NBL33.import_scan_data(data_path)
 TS58A.import_scan_data(data_path)
 # "sample.h5data" now exists: holds h5 files
 
 # calc factor (cts-->ampere) for XBIC channel in each h5
+NBL31.get_lockin(data_path+'/a_class_electrical.csv')
 NBL32.get_lockin(data_path+'/a_class_electrical.csv')
 NBL33.get_lockin(data_path+'/a_class_electrical.csv')
 TS58A.get_lockin(data_path+'/a_class_electrical.csv')
@@ -60,6 +70,7 @@ TS58A.get_lockin(data_path+'/a_class_electrical.csv')
 # arg3: scaler channel to normalize elemental signal
 # arg4: use 'fit' on fitted h5s, or 'roi' for unfitted h5s
 elements = ['Cu', 'Cd_L', 'Te_L']
+NBL31.import_maps('ds_ic', elements, 'us_ic', 'fit')
 NBL32.import_maps('ds_ic', elements, 'us_ic', 'fit')
 NBL33.import_maps('ds_ic', elements, 'us_ic', 'fit')
 TS58A.import_maps('ds_ic', elements, 'us_ic', 'fit')
