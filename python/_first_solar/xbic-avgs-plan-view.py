389289981dd417af53dd9d8c533c7416a8160e57
tzwalker <tzwalker@asu.edu>
2020-04-24 20:42:51 -0700

    xbic plan averages

--- python/_first_solar/xbic-avgs-plan-view.py
+++ python/_first_solar/xbic-avgs-plan-view.py
@@ -20,3 +20,14 @@ p = [25,50,75] # percentiles
 xbic_percentiles = [np.percentile(array, p) for array in xbic_maps]
 percentiles = np.array(xbic_percentiles)
 #merge = np.concatenate((maxes.reshape(-1,1),avgs.reshape(-1,1)), axis=1)
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+xbic_maps = [scan[0,:,:-2] for scan in FS3.maps]
+
+for array in xbic_maps:
+    plt.figure()
+    plt.imshow(array,vmin=5.6E-8,vmax=9E-8)
+
