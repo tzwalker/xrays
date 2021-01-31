e611f32968e5c4d81092f88740f6f1f477be3d23
tzwalker <tzwalker@asu.edu>
2021-01-31 09:58:39 -0700

    add PL map processing

--- python/_PVSe33/plot_quick_PL.py
+++ python/_PVSe33/plot_quick_PL.py
@@ -11,33 +11,38 @@ this program is meant to process the .txt PL and Raman data
 
 '''for spectra of different pixels of PL map'''
 import pandas as pd
-import matplotlib.pyplot as plt
+
 
 IN_PATH = r'C:\Users\triton\Dropbox (ASU)\1_PVSe33 ex-situ\PL'
 FNAME = r'\20210119 PVSe33.4_3SLAC PL MAP - Au side test2.txt'
 
 data = pd.read_csv(IN_PATH+FNAME, sep='\t', skiprows=1)
-data.columns = ["X (um)", "Y (um)", "Wave #", "Intensity"]
+data.columns = ["X", "Y", "Wave #", "Intensity"]
 
-data_arr = data.to_numpy()
+map_dict = data.set_index(['X', 'Y']).agg(list, 1).groupby(level=(0,1)).agg(list).to_dict()
 
-i = 1
-
-while data_arr[i,0] == data_arr[i-1,0]:
-    i = i+1
-
-# isolate data of pixxel nca
-    # i looked in the txt file and saw where X (mm) changed
-n = 20
-row_idxLo = 511*n+n
-row_idxHi = 511*(n+1)+n
-data_pixel01 = data.iloc[row_idxLo:row_idxHi]
-
-# extract spectrum
-data_pixel01_spectrum = data_pixel01[data_pixel01.columns[-2:]]
-
-plt.plot(data_pixel01_spectrum["Wave #"], data_pixel01_spectrum["Intensity"])
-
+# =============================================================================
+# data_arr = data.to_numpy()
+# 
+# i = 1
+# 
+# while data_arr[i,0] == data_arr[i-1,0]:
+#     i = i+1
+# 
+# # isolate data of pixxel nca
+#     # i looked in the txt file and saw where X (mm) changed
+# n = 20
+# row_idxLo = 511*n+n
+# row_idxHi = 511*(n+1)+n
+# data_pixel01 = data.iloc[row_idxLo:row_idxHi]
+# 
+# # extract spectrum
+# data_pixel01_spectrum = data_pixel01[data_pixel01.columns[-2:]]
+# 
+# import matplotlib.pyplot as plt
+# plt.plot(data_pixel01_spectrum["Wave #"], data_pixel01_spectrum["Intensity"])
+# 
+# =============================================================================
 #%%
 '''for single spectrum'''
 import pandas as pd
