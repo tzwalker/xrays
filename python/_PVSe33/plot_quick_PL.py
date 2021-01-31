3bf6b5f1b690cd94c4274d94c7dae6fcd85c7141
tzwalker <tzwalker@asu.edu>
2021-01-31 10:09:03 -0700

    PL map processing

--- python/_PVSe33/plot_quick_PL.py
+++ python/_PVSe33/plot_quick_PL.py
@@ -9,8 +9,9 @@ this program is meant to process the .txt PL and Raman data
     
 """
 
-'''for spectra of different pixels of PL map'''
+
 import pandas as pd
+import numpy as np
 
 
 IN_PATH = r'C:\Users\triton\Dropbox (ASU)\1_PVSe33 ex-situ\PL'
@@ -19,18 +20,29 @@ FNAME = r'\20210119 PVSe33.4_3SLAC PL MAP - Au side test2.txt'
 data = pd.read_csv(IN_PATH+FNAME, sep='\t', skiprows=1)
 data.columns = ["X", "Y", "Wave #", "Intensity"]
 
+# create dictionary where each pixel is an entry
+# from StackOverflow solution:
+    # https://stackoverflow.com/questions/65969561/how-do-i-convert-mapped-data-to-dictionary-where-each-xy-coordinate-contains-a/65969669#65969669
 map_dict = data.set_index(['X', 'Y']).agg(list, 1).groupby(level=(0,1)).agg(list).to_dict()
 
+vals = np.fromiter(map_dict.values(), dtype=float)
+
+#for pixel in map_dict:
+pixel_spectrum = map_dict[pixel]
+pixel_spectrum_arr = np.array(pixel_spectrum)
+# fit spectrum to a Gaussian distribution
+# get max amplitude of Gaussian fit
+# copy dictionary key
+# set value of copy dictionary key to max amplitude
+# store this key-value pair in new dictionary
+
+print(np.shape(pixel_spectrum))
+    
+
 # =============================================================================
-# data_arr = data.to_numpy()
-# 
-# i = 1
-# 
-# while data_arr[i,0] == data_arr[i-1,0]:
-#     i = i+1
-# 
-# # isolate data of pixxel nca
-#     # i looked in the txt file and saw where X (mm) changed
+# '''for spectra of different pixels of PL map'''
+# # isolate data of pixel with index, 'n'
+#     # i looked in the txt file and found the index where X changed
 # n = 20
 # row_idxLo = 511*n+n
 # row_idxHi = 511*(n+1)+n
