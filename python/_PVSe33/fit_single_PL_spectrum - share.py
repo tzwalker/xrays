c63588d3746f6a834167ebd4d7aa5967eddcba88
tzwalker <tzwalker@asu.edu>
2021-02-15 14:14:44 -0700

    PL fit modifications

--- python/_PVSe33/fit_single_PL_spectrum - share.py
+++ python/_PVSe33/fit_single_PL_spectrum - share.py
@@ -10,9 +10,6 @@ the programs use either the wdf file or a text file output
 """
 import pandas as pd
 import matplotlib.pyplot as plt
-from astropy import modeling
-from scipy.stats import norm
-from scipy import optimize
 import numpy as np
 
 
@@ -43,7 +40,7 @@ intensity = data[:,1]
 #plt.plot(intensity)
 
 share = data[::20]
-plt.plot(share[:,1])
+plt.plot(share[:,1], 'k', label='original')
 
 # ALS approach
 from scipy import sparse
@@ -61,9 +58,7 @@ def baseline_als(y, lam, p, niter=10):
 
 baseline = baseline_als(share[:,1], 1E6, 0.001)
 baseline_subtracted = share[:,1] - baseline
-plt.plot(baseline_subtracted)
-
-
+plt.plot(baseline_subtracted, 'r', label='als')
 
 # rubberband approach
 from scipy.spatial import ConvexHull
@@ -79,43 +74,64 @@ def rubberband(x, y):
 
 baseline_rubber = rubberband(share[:,0], share[:,1])
 intensity_rubber = share[:,1] - baseline_rubber
-plt.plot(intensity_rubber)
+#plt.plot(intensity_rubber, 'b', label='rubberband')
 
 # peakutils approach
 import peakutils
 baseline_peakutils = peakutils.baseline(share[:,1])
 intensity_peakutils = share[:,1] - baseline_peakutils
-plt.plot(intensity_peakutils)
+plt.plot(intensity_peakutils, 'g', label='peakutils')
 
-# =============================================================================
-# intensity_adjusted = intensity[125:500]
-# zero_intensity = intensity - np.min(intensity , keepdims=True)
-# intensity[501:] = intensity_adjusted - baseline
-# plt.plot(zero_intensity)
-# =============================================================================
+# arpls approach
+from scipy.linalg import cholesky
+def arpls(y, lam=1e4, ratio=0.05, itermax=100):
+    r"""
+    Baseline correction using asymmetrically
+    reweighted penalized least squares smoothing
+    Sung-June Baek, Aaron Park, Young-Jin Ahna and Jaebum Choo,
+    Analyst, 2015, 140, 250 (2015)
+    Inputs:
+        y:
+            input data (i.e. chromatogram of spectrum)
+        lam:
+            parameter that can be adjusted by user. The larger lambda is,
+            the smoother the resulting background, z
+        ratio:
+            wheighting deviations: 0 < ratio < 1, smaller values allow less negative values
+        itermax:
+            number of iterations to perform
+    Output:
+        the fitted background vector
+    """
+    N = len(y)
+#  D = sparse.csc_matrix(np.diff(np.eye(N), 2))
+    D = sparse.eye(N, format='csc')
+    D = D[1:] - D[:-1]  # numpy.diff( ,2) does not work with sparse matrix. This is a workaround.
+    D = D[1:] - D[:-1]
 
-# =============================================================================
-# data_base = data[:,1] - np.min(data[:,1] , keepdims=True)
-# peaks_a = peak_in_range(data_base, data[:,0], [1.37, 1.43])
-# =============================================================================
+    H = lam * D.T * D
+    w = np.ones(N)
+    for i in range(itermax):
+        W = sparse.diags(w, 0, shape=(N, N))
+        WH = sparse.csc_matrix(W + H)
+        C = sparse.csc_matrix(cholesky(WH.todense()))
+        z = spsolve(C, spsolve(C.T, w * y))
+        d = y - z
+        dn = d[d < 0]
+        m = np.mean(dn)
+        s = np.std(dn)
+        wt = 1. / (1 + np.exp(2 * (d - (2 * s - m)) / s))
+        if np.linalg.norm(w - wt) / np.linalg.norm(w) < ratio:
+            break
+        w = wt
+    return z
 
-# =============================================================================
-# # gaussian fit try 1
-# popt, _ = optimize.curve_fit(gaussian, data[:,0], data[:,1])
-# plt.plot(data[:,0], data[:,1])
-# plt.plot(data[:,0], gaussian(data[:,0], *popt))
-# 
-# # gaussian fit try 2
-# mean,std=norm.fit(data[:,1])
-# px_fit = norm.pdf(data[:,0], mean, std)
-# plt.plot(data[:,0], data[:,1])
-# plt.plot(data[:,0], px_fit)
-# 
-# # gaussian fit try 3
-# fitter = modeling.fitting.LevMarLSQFitter()
-# # depending on the data you need to give some initial values
-# model = modeling.models.Gaussian1D()   
-# fitted_model = fitter(model, data[:,0], data[:,1])
-# plt.plot(data[:,0], data[:,1])
-# plt.plot(data[:,0], fitted_model(data[:,0]))
-# =============================================================================
+baseline = baseline_als(share[:,1], 1E6, 0.001)
+baseline_subtracted = share[:,1] - baseline
+plt.plot(baseline_subtracted, 'r', label='als')
+
+baseline_arpls = arpls(share[:,1], 1e5, 0.1)
+intensity_arpls = share[:,1] - baseline_arpls
+plt.plot(intensity_arpls, label='arpls')
+
+plt.legend()
--- python/_PVSe33/fit_single_PL_spectrum.py
+++ python/_PVSe33/fit_single_PL_spectrum.py
@@ -4,15 +4,14 @@ coding: utf-8
 tzwalker
 Wed Feb  3 10:07:35 2021
 
-these programs are used as a testbed for fitting PL spectra from
-    the Renishaw tool
-the programs use either the wdf file or a text file output
+this program i smeant to import and fit a single PL spectrum
+from the Renishaw tool
+
+it assumes you have exported the file as a .txt file
 """
 import pandas as pd
 import matplotlib.pyplot as plt
-#from astropy import modeling
-#from scipy.stats import norm
-#from scipy import optimize
+from scipy import optimize
 import numpy as np
 
 
@@ -25,14 +24,14 @@ def peak_in_range(spectra, wn, range_list, method="max", **params):
     return getattr(np, method)(spectra_cut, **params)
 
 
-
-def gaussian(x, y0, amplitude, mean, stddev):
-    return y0 + amplitude * np.exp(-((x - mean) / 4 / stddev)**2)
+def gaussian(x, height, center, width, offset):
+    return height*np.exp(-(x - center)**2/(2*width**2)) + offset
 
 
 '''for single spectrum'''
 IN_PATH = r'C:\Users\triton\Dropbox (ASU)\1_PVSe33 ex-situ\PL'
 FNAME = r'\20210119 PVSe33.4_3SLAC PL - glass side test1.txt'
+#FNAME = r'\20201105 PVSe33.3_4 PL Spot - Au side - boundary.txt'
 
 spectrum = pd.read_csv(IN_PATH+FNAME, sep='\t', skiprows=1)
 spectrum.columns = ["Wave #", "Intensity"]
@@ -40,83 +39,42 @@ spectrum.columns = ["Wave #", "Intensity"]
 data = spectrum.to_numpy()
 energy = data[:,0]
 intensity = data[:,1]
-plt.plot(intensity)
+plt.plot(energy, intensity)
 
-# ALS approach
-from scipy import sparse
-from scipy.sparse.linalg import spsolve
-def baseline_als(y, lam, p, niter=10):
-    L = len(y)
-    D = sparse.csc_matrix(np.diff(np.eye(L), 2))
-    w = np.ones(L)
-    for i in range(niter):
-      W = sparse.spdiags(w, 0, L, L)
-      Z = W + lam * D.dot(D.transpose())
-      z = spsolve(Z, w*y)
-      w = p * (y > z) + (1-p) * (y < z)
-    return z
+from baseline_algorithms import arpls
+baseline_arpls = arpls(intensity, 1e5, 0.1)
+intensity_arpls = intensity - baseline_arpls
+plt.plot(energy, intensity_arpls)
 
-baseline = baseline_als(intensity, 1E6, 0.001)
-baseline_subtracted = intensity - baseline
-plt.plot(baseline_subtracted)
-# try to repeat
-baseline1 = baseline_als(baseline_subtracted, 1E6, 0.01)
-plt.plot(baseline1)
-baseline_subtracted1 = baseline_subtracted - baseline1
-plt.plot(baseline_subtracted1)
-
-#%%
-# rubberband approach
-from scipy.spatial import ConvexHull
-def rubberband(x, y):
-    # Find the convex hull
-    v = ConvexHull(data).vertices
-    # Rotate convex hull vertices until they start from the lowest one
-    v = np.roll(v, v.argmax())
-    # Leave only the ascending part
-    v = v[:v.argmax()]
-    # Create baseline using linear interpolation between vertices
-    return np.interp(x, x[v], y[v])
-
-baseline_rubber = rubberband(energy, intensity)
-intensity_rubber = baseline_subtracted - baseline_rubber
-plt.plot(intensity_rubber)
-
-# peakutils approach
-import peakutils
-baseline_peakutils = peakutils.baseline(share[:,1])
-intensity_peakutils = share[:,1] - baseline_peakutils
-plt.plot(intensity_peakutils)
-
-# =============================================================================
-# intensity_adjusted = intensity[125:500]
-# zero_intensity = intensity - np.min(intensity , keepdims=True)
-# intensity[501:] = intensity_adjusted - baseline
-# plt.plot(zero_intensity)
-# =============================================================================
 
 # =============================================================================
 # data_base = data[:,1] - np.min(data[:,1] , keepdims=True)
 # peaks_a = peak_in_range(data_base, data[:,0], [1.37, 1.43])
 # =============================================================================
 
-# =============================================================================
-# # gaussian fit try 1
-# popt, _ = optimize.curve_fit(gaussian, data[:,0], data[:,1])
-# plt.plot(data[:,0], data[:,1])
-# plt.plot(data[:,0], gaussian(data[:,0], *popt))
-# 
-# # gaussian fit try 2
-# mean,std=norm.fit(data[:,1])
-# px_fit = norm.pdf(data[:,0], mean, std)
-# plt.plot(data[:,0], data[:,1])
-# plt.plot(data[:,0], px_fit)
-# 
-# # gaussian fit try 3
-# fitter = modeling.fitting.LevMarLSQFitter()
-# # depending on the data you need to give some initial values
-# model = modeling.models.Gaussian1D()   
-# fitted_model = fitter(model, data[:,0], data[:,1])
-# plt.plot(data[:,0], data[:,1])
-# plt.plot(data[:,0], fitted_model(data[:,0]))
-# =============================================================================
+
+# gaussian fit try 1
+popt, _ = optimize.curve_fit(gaussian, energy, intensity_arpls)
+plt.plot(energy, intensity_arpls)
+plt.plot(energy, gaussian(energy, *popt))
+
+# multi-gaussian fit 2
+#https://stackoverflow.com/questions/54851012/fitting-data-with-multiple-gaussian-profiles-in-python
+def gaussian2(x, A, x0, sig):
+    return A*np.exp(-(x-x0)**2/(2*sig**2))
+
+def multi_gaussian(x, *pars):
+    offset = pars[0]
+    g1 = gaussian2(x, pars[1], pars[2], pars[3])
+    g2 = gaussian2(x, pars[4], pars[5], pars[6])
+    return g1 + g2 + offset
+
+# Initial guesses for the parameters to fit:
+# 3 amplitudes, means and standard deviations plus a continuum offset.
+guess = [8.5, 2525, 1.39, 0.013, 500, 1.37, 0.01]
+popt, pcov = optimize.curve_fit(multi_gaussian, energy, intensity_arpls, guess)
+plt.plot(energy, intensity_arpls, label='Data')
+g1_plus_g2 = multi_gaussian(energy, *popt)
+plt.plot(energy, g1_plus_g2, 'r--', label='Fit')
+plt.legend()
+
