33e5b76e3131794ad8671ad5a9686a05eb54a2d0
tzwalker <tzwalker@asu.edu>
2021-11-14 22:03:47 -0700

    simplified XRF plotting stage paper

--- python/_first_solar/class_ascii_Sample.py
+++ python/_first_solar/class_ascii_Sample.py
@@ -87,6 +87,34 @@ class Sample():
             setattr(self, name, data_stack) # useful for plotting & reference
             self.maps.append(data_stack) #useful w/ code before 20200402
     
+    def stage_paper_XRF(self, path_ascii, CH):
+        '''
+        this def is needed to import ASCII from BLai fits
+        to import raw ASCII:
+        x pixel no and y pixel no need to have a space in their string
+        the first row needs to be skipped
+        
+        '''
+        # store scan data
+        for scan in self.scans:
+            scan_str = str(scan)
+            # define ascii of scan
+            file = path_ascii + '/combined_ASCII_2idd_0'+scan_str+'.h5.csv'
+            # import ascii as dataframe
+            data = pd.read_csv(file, skiprows=1)
+            # dataframe keys used for shaping into map
+            i = ' y pixel no'; j='x pixel no'
+            # extract maps of interest; shape according to pixel no
+            data_shape = [data.pivot(index=i, columns=j, values=c) for c in CH]
+            # convert dataframes to numpy arrays
+            data_arr = [df.to_numpy() for df in data_shape]
+            # stack numpy arrays (into 3D)
+            data_stack = np.array(data_arr)
+            # store stacked numpy arrays
+            name = 'scan' + scan_str
+            setattr(self, name, data_stack) # useful for plotting & reference
+            self.maps.append(data_stack) #useful w/ code before 20200402
+    
     def ug_to_mol(self, elements):
         # get the XRF maps from each scan
         XRF_maps = [scan[-1:,:,:-2] for scan in self.maps]
--- python/_first_solar/cts_per_s_XRF/main-FS3-ASCII.py
+++ python/_first_solar/cts_per_s_XRF/main-FS3-ASCII.py
@@ -4,24 +4,16 @@ tzwalker
 Sat Nov  13 17:12:40 2021
 coding: utf-8
 
-this program imports the data from two fits,
-one performed by TWalker and the other from BLai
+this program imports the data from one fit
 
-two fits were needed to recover the upstream ion chamber channel
+i just need the XRF normalized to the US_IC
+for the stage paper
 
-these programs were run before this program
-    workflow:
-        1. 'ASCII-merge.py'
+everything is hard coded, including the alignment of the XRF maps between
+the two scans; i have the translation coordinates from the SIFT alignment
+in 'XBIC-translate'
 
-after (1), ASCIIS_TW_BL have the combined data
-    'us_ic' is the relevant scaler for the electrical channel
-    'US_IC' is the relevant scaler for the upstream ion chamber
-
-workflow to align XBIC and XBIV maps:
-    1. 'main-FS3-ASCII'
-    2. '...-translate-and-deltas'
-
-
+use the other directory (ug_per_cm2_XRF) for aligning all the data
 
 for plotting reference, pixel step was 
 
@@ -29,15 +21,12 @@ for plotting reference, pixel step was
 67pix = 10um
 
 """
-
-# after (3), the aligned maps were saved as 2D csvs  in
-    # r'C:\Users\triton\Dropbox (ASU)\2_FS_operando\XBIC aligned image csvs\cts_per_s_XRF'
+import sys
+sys.path.append(r'C:\Users\triton\xrays\python\_first_solar')
 
 from class_ascii_Sample import Sample
-#ASCII_PATH = r'C:\Users\triton\FS3_2019_06_operando\ASCIIS_TW_BL'
+
 ASCII_PATH = r'C:\Users\triton\FS3_2019_06_operando\BL_fit_202002\output'
-#ASCII_PATH =  r'C:\Users\Trumann\FS3_2019_06_operando\ASCIIS_TW_BL' 
-#PATH_LOCKIN = r'C:\Users\Trumann\FS3_2019_06_operando\ASCIIS_TW_BL\FS_plan_electrical.csv'
 
 # create sample objects
 FS3 = Sample()
@@ -47,125 +36,117 @@ FS3.stack = {'Au':   [19.3, 100E-7],
                  'CdTe': [5.85, 5E-4],
                  'Se': [4.82, 100E-7],
                  'SnO2': [100E-7]}
-FS3.scans = [323,339] #XBIC: [323,327,332,339,344] #XBIV: [321,325,330,337,342]
+FS3.scans = [323,339]
 
 
 # channels to import from ASCII
-channels = ['US_IC', 'Se', 'Cd_L', 'Te_L', 'Au_L']
+channels = [' US_IC', ' Se', ' Cd_L', ' Te_L', ' Au_L']
 
-# uncomment this line to import maps with XBIC converted to ampere
-# this requires XBIC channel ('us_ic') to be in first position of 'channels' list
-#FS3.import_maps(ASCII_PATH, PATH_LOCKIN, channels)
 
 # uncomment this line to import maps without XBIC converted to ampere
-FS3.import_maps_no_XBIC_conversion(ASCII_PATH, channels)
+FS3.stage_paper_XRF(ASCII_PATH, channels)
 
 #%%
-'''
-current maps
 
-this cell was used to normalize the XBIC maps to the 
-upstream ion chamber
-to do this, use Sample class method 'import_maps_no_XBIC_conversion' above
-	this keeps XBIC in cts/s
+'''this cell is from XBIC-translate-and-deltas.py'''
 
-only run this cell if XBIC scans [323,327,...] are imported 
-at the beginning of this program
-
-it's important to normalize the XBIC channel ('us_ic') cts/s 
-to upstream ion chamber ('US_IC') cts/s 
-since the incident flux can change over the time of the experiment (up to 40hrs)
-	especially during fill/unfill
-
-normalization needs to be done before converting into ampere
-	
-'''
-
-# normalize the xbic channel to the us_ic channel
-xboc_norms = []
-for scan in FS3.maps:
-    us_ic = scan[0,:,:-2]
-    xbic = scan[1,:,:-2]
-    xbic_norm = xbic / us_ic
-    xboc_norms.append(xbic_norm)
-
-# turn maps in to vectors
+from skimage.transform import SimilarityTransform, warp
 import numpy as np
-xbic_norms_ravel = [np.ravel(xbic_norm) for xbic_norm in xboc_norms]
 
-# remove bad row in 100C map
-xbic_norms_ravel[4] = np.delete(aligned_crop[4], 38, axis=1)
+### start with map with largest offset
+img2 = FS3.scan339
+SHFT2 = SimilarityTransform(translation=(-11, -8))
+# shift (translate) all imported channels
+shifted_channels2 = []
+for i, channel in enumerate(channels):
+    IMG2_SHIFT = warp(img2[i,:,:-2], SHFT2)
+    shifted_channels2.append(IMG2_SHIFT)
+# store shifted channels
+shifted_channels2 = np.array(shifted_channels2)
 
-# turn list of vectors to nupmy array
-xbic_norms_array = np.array(xbic_norms_ravel).T
+### make mask that defines area to crop
+mask = IMG2_SHIFT!=0
+mask = mask.astype(int)
 
-OUT_PATH = r'C:\Users\triton\Dropbox (ASU)\2_FS_operando\histogram arrays from python'
-FNAME = r'\arrays_for_hist_normalizedXBICvTemp.csv'
-np.savetxt(OUT_PATH+FNAME, xbic_norms_array, delimiter=",")
+### mask the map used as alignemnt reference
+img0 = FS3.scan323
+shifted_channels0 = []
+for i, channel in enumerate(channels):
+    # mask the translated maps
+    IMG0_MSK = img0[i,:,:-2]*mask
+    shifted_channels0.append(IMG0_MSK)
+shifted_channels0 = np.array(shifted_channels0) 
 
-#%%
-'''
-voltage maps 
+### store aligned and masked images
+aligned = [shifted_channels0,
+           shifted_channels2]
 
-this cell was used to normalize the XBIV maps to the 
-upstream ion chamber; to do this, the XBIV channel
-was kept in cts/s, hence the use of the Sample class method
-'import_maps_no_XBIC_conversion'
-
-only run this cell if the XBIV scans are imported 
-at the beginning of this program
-'''
-# it's important to normalize the XBIC channel 'us_ic' cts/s to the
-# upstream ion chamber 'US_IC' cts/s before converting into ampere
-    # the measurements took place over many hours and the incident beam
-    # flux can easily change during that time (especially during fill/unfill)
-
-FS3.import_maps_no_XBIC_conversion(ASCII_PATH, channels)
-
-# normalize the xbic channel to the us_ic channel
-xboc_norms = []
-for scan in FS3.maps:
-    us_ic = scan[0,:,:-2]
-    xbic = scan[1,:,:-2]
-    xbic_norm = xbic / us_ic
-    xboc_norms.append(xbic_norm)
-
-# turn maps in to vectors
-import numpy as np
-xbic_norms_ravel = [np.ravel(xbic_norm) for xbic_norm in xboc_norms]
-
-# turn list of vectors to nupmy array
-xbic_norms_array = np.array(xbic_norms_ravel).T
-
-OUT_PATH = r'C:\Users\triton\Dropbox (ASU)\2_FS_operando\histogram arrays from python'
-FNAME = r'\arrays_for_hist_normalizedXBIVvTemp.csv'
-np.savetxt(OUT_PATH+FNAME, xbic_norms_array, delimiter=",")
+### remove zeros according to indices of map with largest offsets
+# in the XBIC images, the last 100C map is cut off both above and below, 
+    # from row indices 19:176
+aligned_crop = [arr[:,8:,11:] for arr in aligned]
 
 #%%
 '''
-this cell is backup in case i want to do an XRF absorption correction 
-for the FS3 sample measured in the new operando stage 2019_06_2IDD
+this cell is for plotting the XRF data normalized to the US_IC
+the US_IC and XRF came from the same fitted data file fit by BLai
+
+
+Cd range - vmin=0.00,vmax=15.0
+Te range - vmin=0.00,vmax=15.0
+Se range - vmin=0.70,vmax=1.50
+
 '''
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.ticker as mticker
+
+# specify index of scan, cmap, and fname
+# 0 is scan323, 1 is scan339
+idx = 1
+
+channel = 4
+
+T_list = ['20C','80C']
+scans1 = [323,339]
+
+img = aligned_crop[idx][channel,:,:]
+
+fig, ax = plt.subplots(figsize=(1.8,3.6))
+
+im = ax.imshow(img, cmap = 'copper', origin='lower',vmin=9,vmax=15.0)
+
+# format tick labels (convert to um)
+fmtr_x = lambda x, pos: f'{(x * 0.150):.0f}'
+fmtr_y = lambda x, pos: f'{(x * 0.150):.0f}'
+ax.xaxis.set_major_formatter(mticker.FuncFormatter(fmtr_x))
+ax.yaxis.set_major_formatter(mticker.FuncFormatter(fmtr_y))
+
+# plot some arbitrary points at same locations
+plt.scatter([50,200,75], [120,60,25], facecolors='none', edgecolors='black', marker='o', s=10)
 
 
-#elements = [ele[0:2] for ele in channels[1:]]
+ax.set_xlabel("X (um)", size=8)
+ax.xaxis.set_tick_params(labelsize=8)
 
-#FS3.ug_to_mol(elements)
-# attribute Sample.mol now exists; contains XRF maps as mol/cm2
+ax.set_ylabel("Y (um)", size=8)
+ax.yaxis.set_tick_params(labelsize=8)
 
-#i want to apply a Se grading correction based off a function 
-# from the cross-section integrated XRF profiles
-    # the two cross-section maps from which to 
-    # get integrated profiles:
-        # FS2_1, 2019_03_2IDD, scan551
-        # FS2_2, 2019_03_2IDD, scan661
-    # 
-#beam_settings = {'beam_energy': 12.7, 'beam_theta':75, 'detect_theta':15}
+divider = make_axes_locatable(ax)
 
-#FS3iios = XRFcorr.get_iios(beam_settings, elements, FS3.stack, end_layer='Se')
-#scans_for_correction = FS3.scans
-#FS3.apply_iios(scans_for_correction, FS3iios)
+cax = divider.new_vertical(size='5%', pad=0.1)
+fig.add_axes(cax)
+cbar = fig.colorbar(im, cax=cax, orientation='horizontal')
+cbar.set_label('Au XRF Intensity (arb. units)', rotation=0, fontsize=8)
+cbar.ax.locator_params(nbins=4)
+cbar.ax.tick_params(labelsize=8)
+cax.xaxis.set_label_position('top')
+cax.xaxis.set_ticks_position('top')
 
-# i need to modify the apply_iios definition in Sample() class
-# in order to handle an Se gradient
+
+
+OUT_PATH = r'C:\Users\triton\Dropbox (ASU)\0_stage design\20211114 figures_v2\figure4 materials'
+FNAME = r'\FS3_{TEMP}_scan{SCAN}_{CHAN}.eps'.format(TEMP=T_list[idx], SCAN=scans1[idx], CHAN=channels[channel])
+print(FNAME)
+#plt.savefig(OUT_PATH+FNAME, format='eps', dpi=300, bbox_inches='tight', pad_inches = 0)
 
