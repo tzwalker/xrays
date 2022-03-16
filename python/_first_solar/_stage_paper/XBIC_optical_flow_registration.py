bfa84159362dd5dc5771c10207513740a929fb0c
tzwalker <tzwalker@asu.edu>
2022-03-16 12:17:51 -0700

    optical_flow_registration attempt

---
+++ python/_first_solar/_stage_paper/XBIC_optical_flow_registration.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+
+Trumann
+Wed Mar 16 11:58:56 2022
+
+https://scikit-image.org/docs/dev/auto_examples/registration/plot_opticalflow.html#sphx-glr-auto-examples-registration-plot-opticalflow-py
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+from skimage.color import rgb2gray
+from skimage.data import stereo_motorcycle, vortex
+from skimage.transform import warp
+from skimage.registration import optical_flow_tvl1, optical_flow_ilk
+
+# --- Load the sequence
+image0 = np.loadtxt(r"C:\Users\Trumann\Dropbox (ASU)\2_FS_operando\for_imageJ\XBIC\from_python\scan323_XBIC.txt")
+image0 = image0 / image0.max()
+image1 = np.loadtxt(r"C:\Users\Trumann\Dropbox (ASU)\2_FS_operando\for_imageJ\XBIC\from_python\scan339_XBIC.txt")
+image1 = image1 / image1.max()
+
+# --- Convert the images to gray level: color is not supported.
+image0 = rgb2gray(image0)
+image1 = rgb2gray(image1)
+
+# --- Compute the optical flow
+v, u = optical_flow_tvl1(image0, image1)
+
+# --- Use the estimated optical flow for registration
+
+nr, nc = image0.shape
+
+row_coords, col_coords = np.meshgrid(np.arange(nr), np.arange(nc),
+                                     indexing='ij')
+
+image1_warp = warp(image1, np.array([row_coords + v, col_coords + u]),
+                   mode='edge')
+
+# build an RGB image with the unregistered sequence
+seq_im = np.zeros((nr, nc, 3))
+seq_im[..., 0] = image1
+seq_im[..., 1] = image0
+seq_im[..., 2] = image0
+
+# build an RGB image with the registered sequence
+reg_im = np.zeros((nr, nc, 3))
+reg_im[..., 0] = image1_warp
+reg_im[..., 1] = image0
+reg_im[..., 2] = image0
+
+# build an RGB image with the registered sequence
+target_im = np.zeros((nr, nc, 3))
+target_im[..., 0] = image0
+target_im[..., 1] = image0
+target_im[..., 2] = image0
+
+# --- Show the result
+
+fig, (ax0, ax1, ax2) = plt.subplots(3, 1, figsize=(5, 10))
+
+ax0.imshow(seq_im)
+ax0.set_title("Unregistered sequence")
+ax0.set_axis_off()
+
+ax1.imshow(reg_im)
+ax1.set_title("Registered sequence")
+ax1.set_axis_off()
+
+ax2.imshow(target_im)
+ax2.set_title("Target")
+ax2.set_axis_off()
+
+fig.tight_layout()
