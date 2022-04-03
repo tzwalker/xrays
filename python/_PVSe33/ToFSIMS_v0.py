6d276a633fed55afc0c2f3800eed511b22799978
tzwalker <tzwalker@asu.edu>
2022-04-02 18:09:48 -0700

    ToF-SIMS processing

--- python/_PVSe33/ToFSIMS_v0.py
+++ python/_PVSe33/ToFSIMS_v0.py
@@ -17,18 +17,19 @@ import numpy as np
 
 f = r"C:\Users\Trumann\Dropbox (ASU)\1_PVSe33 ex-situ\DATA\ToF_SIMS\33.3.2_tomog_ (7) - Cl-.txt"
 
-data = np.loadtxt(f,skiprows=10)[0:512]
+data = np.loadtxt(f)
+indices = data[:,0:3].copy().astype('int')
+values = data[:,3].copy()
 
-# =============================================================================
-# #data = data.reshape((512,512,49))
-# 
-# dat = data[:,:-1]
-# 
-# #dat = dat.reshape((512,512,49))
-# 
-# import pandas as pd
-# 
-# new = pd.DataFrame(data=data[1:,1:],index=data[1:,0],columns=data[0,1:])
-# 
-# new1 = pd.pivot(index=, columns, values)
-# =============================================================================
+res = np.zeros((49,512,512),float)
+
+res[indices[:,2],indices[:,0],indices[:,1]] = values
+
+import matplotlib.pyplot as plt
+
+slices = [0,10,20,30,40]
+for s in slices:
+    image = res[s,:,:]
+    plt.figure()
+    plt.imshow(image)
+
