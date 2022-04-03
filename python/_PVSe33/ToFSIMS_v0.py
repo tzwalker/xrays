41eb3bae675fb72f0276008f53d5a5a7c09e8e85
tzwalker <tzwalker@asu.edu>
2022-04-02 18:48:27 -0700

    reducing ToF-SIMS array

--- python/_PVSe33/ToFSIMS_v0.py
+++ python/_PVSe33/ToFSIMS_v0.py
@@ -25,11 +25,23 @@ res = np.zeros((49,512,512),float)
 
 res[indices[:,2],indices[:,0],indices[:,1]] = values
 
-import matplotlib.pyplot as plt
+res = res.astype('int')
+# =============================================================================
+# import matplotlib.pyplot as plt
+# 
+# slices = [0,10,20,30,40]
+# for s in slices:
+#     image = res[s,:,:]
+#     plt.figure()
+#     plt.imshow(image)
+# =============================================================================
 
-slices = [0,10,20,30,40]
-for s in slices:
-    image = res[s,:,:]
-    plt.figure()
-    plt.imshow(image)
+#import cv2
 
+#img = cv2.merge((res[2],res[1],res[0]))
+PATH_OUT = r"C:\Users\Trumann\Dropbox (ASU)\1_PVSe33 ex-situ\DATA\ToF_SIMS\0hr_Cl_map.tif"
+#cv2.imwrite(PATH_OUT,img)
+
+import tifffile
+
+tifffile.imsave(PATH_OUT, res, bigtiff=True)
